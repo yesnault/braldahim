a3768840464747f0d9d0eded2f45af3cdf92e3c1
BouleHondevice <boule.hondevice@braldahim.com>
2007-05-19 18:35:18 +0000

    Finalisation Essene Cehef
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@56 8885cbc4-9223-0410-882a-37b6fa6ce781

--- braldahim/library/Bral/Lieux/Essenecehef.php
+++ braldahim/library/Bral/Lieux/Essenecehef.php
@@ -4,20 +4,67 @@ class Bral_Lieux_Essenecehef extends Bral_Lieux_Lieu {
 
 	private $_utilisationPossible = false;
 	private $_coutCastars = null;
+	private $_tabDestinations = null;
 
 	function prepareCommun() {
+		Zend_Loader::loadClass("Lieu");
 		$this->_coutCastars = $this->calculCoutCastars();
 		$this->_utilisationPossible = (($this->view->user->castars_hobbit -  $this->_coutCastars) > 0);
+
+		$lieuTable = new Lieu();
+		$esseneCehefRowset = $lieuTable->findByType($this->view->config->game->lieu->type->essene_cehef);
+
+		foreach($esseneCehefRowset as $e) {
+			if ($e->x_lieu == $this->view->user->x_hobbit && $e->y_lieu == $this->view->user->y_hobbit) {
+				// on ne propose pas le lieu ou le hobbit est present
+			} else {
+				$this->_tabDestinations[] = array("id" => $e->id, "nom" => $e->nom_lieu, "x" => $e->x_lieu, "y" => $e->y_lieu) ;
+			}
+		}
 	}
 
 	function prepareFormulaire() {
-		
+		$this->view->tabDestinations = $this->_tabDestinations;
 		$this->view->utilisationPossible = $this->_utilisationPossible;
 		$this->view->coutCastars = $this->_coutCastars;
 	}
 
 	function prepareResultat() {
+		$idDestination = intval($this->request->get("valeur_1"));
+		$xDestination = null;
+		$yDestination = null;
 		
+		// verification qu'il y a assez de castars
+		if ($this->_utilisationPossible == false) {
+			throw new Zend_Exception(get_class($this)." Achat impossible : castars:".$this->view->user->castars_hobbit." cout:".$this->_coutCastars);
+		}
+		
+		// verification que la destination etait bien dans la liste des destinations proposees
+		$destinationOk = false;
+		foreach($this->_tabDestinations as $d) {
+			if ($d["id"] == $idDestination) {
+				$xDestination = $d["x"];
+				$yDestination = $d["y"];
+				$destinationOk = true;
+				break;
+			}
+		}
+
+		if ($destinationOk === false) {
+			throw new Zend_Exception(get_class($this)."::Destination invalide:".$idDestination);
+		}
+
+		$hobbitTable = new Hobbit();
+		$this->view->user->x_hobbit = $xDestination;
+		$this->view->user->y_hobbit = $yDestination;
+		$this->view->user->castars_hobbit = $this->view->user->castars_hobbit - $this->_coutCastars;
+
+		$data = array('x_hobbit' => $this->view->user->x_hobbit,
+		'y_hobbit' => $this->view->user->y_hobbit,
+		'castars_hobbit' => $this->view->user->castars_hobbit);
+		
+		$where = "id=".$this->view->user->id;
+		$hobbitTable->update($data, $where);
 	}
 
 
@@ -26,6 +73,6 @@ class Bral_Lieux_Essenecehef extends Bral_Lieux_Lieu {
 	}
 
 	private function calculCoutCastars() {
-		return 10;
+		return 50;
 	}
 }
