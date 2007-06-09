f3d46fb287cc36afd4c29039492cda3bcfd109d6
BouleHondevice <boule.hondevice@braldahim.com>
2007-06-09 13:01:31 +0000

    Avancement
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@84 8885cbc4-9223-0410-882a-37b6fa6ce781

--- braldahim/library/Bral/Competences/Distribuerpx.php
+++ braldahim/library/Bral/Competences/Distribuerpx.php
@@ -36,7 +36,73 @@ class Bral_Competences_Distribuerpx extends Bral_Competences_Competence {
 	}
 
 	function prepareResultat() {
-		// todo
+		$tabDistribution = null;
+		$total_distribution = 0;
+		for ($i=2 ;$i<=$this->view->n_hobbits*2; $i=$i+2) {
+			$tab["px_recu"] = 0;
+			$tab["id_hobbit"] = (int)$this->request->get("valeur_".($i-1));
+			if (((int)$this->request->get("valeur_".$i).""!=$this->request->get("valeur_".$i)."")) {
+				throw new Zend_Exception(get_class($this)." Valeur invalide : i=$i val=".$this->request->get("valeur_".$i));
+			} else {
+				$tab["px_recu"] = (int)$this->request->get("valeur_".$i);
+			}
+
+			$trouve = false;
+			foreach($this->view->tabHobbits  as $h) {
+				if ($tab["id_hobbit"] == $h["id_hobbit"]) {
+					$trouve = true;
+				}
+			}
+			if ($trouve == false) {
+				throw new Zend_Exception(get_class($this)." Hobbit invalide : o:".$this->view->user->id_hobbit." d:".$tab["id_hobbit"]);
+			}
+			if ($tab["px_recu"] > 0) {
+				$tabDistribution[] = $tab;
+			}
+				
+			$total_distribution = $total_distribution + $tab["px_recu"];
+		}
+
+		if ($total_distribution > $this->view->user->px_commun_hobbit) {
+			throw new Zend_Exception(get_class($this)." Total trop eleve:".$total_distribution. " c=".$this->view->user->px_commun_hobbit);
+		}
+
+		// distribution
+		$tabAffiche = null;
+		$hobbitTable = new Hobbit();
+		foreach ($tabDistribution as $t) {
+			$hobbitRowset = $hobbitTable->find($t["id_hobbit"]);
+			$hobbit = $hobbitRowset->current();
+			$hobbit->px_perso_hobbit = $hobbit->px_perso_hobbit + $t["px_recu"];
+			$data = array(
+			'px_perso_hobbit' => $hobbit->px_perso_hobbit,
+			);
+			$where = "id_hobbit=".$t["id_hobbit"];
+			$hobbitTable->update($data, $where);
+			if ($t["id_hobbit"] == $this->view->user->id_hobbit) {
+				$this->view->user->px_perso_hobbit = $hobbit->px_perso_hobbit;
+			}
+			$tab["id_hobbit"] = $t["id_hobbit"];
+			if ($t["id_hobbit"] == $this->view->user->id_hobbit) {
+				$tab["nom_hobbit"] = "Vous-Même : ".$hobbit->nom_hobbit;
+			} else {
+				$tab["nom_hobbit"] = $hobbit->nom_hobbit;
+			}
+			$tab["px_recu"] = $t["px_recu"];
+			$tabAffiche[] = $tab;
+		}
+
+		$hobbitRowset = $hobbitTable->find($this->view->user->id_hobbit);
+		$hobbit = $hobbitRowset->current();
+		$this->view->user->px_commun_hobbit = $this->view->user->px_commun_hobbit - $total_distribution;
+		$data = array(
+		'px_commun_hobbit' => $this->view->user->px_commun_hobbit,
+		);
+		$where = "id_hobbit=".$this->view->user->id_hobbit;
+		$hobbitTable->update($data, $where);
+		
+		$this->view->tabAffiche = $tabAffiche;
+		$this->view->totalDistribution = $total_distribution;
 	}
 
 	function getListBoxRefresh() {
