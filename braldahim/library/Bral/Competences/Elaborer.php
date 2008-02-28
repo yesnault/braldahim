27cca64cd1e3edb81fded6cf0c2fdf3a1546bed5
BouleHondevice <boule.hondevice@braldahim.com>
2008-02-28 22:54:05 +0000

    Avancement
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@243 8885cbc4-9223-0410-882a-37b6fa6ce781

--- braldahim/library/Bral/Competences/Elaborer.php
+++ braldahim/library/Bral/Competences/Elaborer.php
@@ -5,8 +5,9 @@ class Bral_Competences_Elaborer extends Bral_Competences_Competence {
 	function prepareCommun() {
 		Zend_Loader::loadClass("Echoppe");
 
-		$id_type_courant = $this->request->get("type_equipement");
-
+		$id_type_courant = $this->request->get("type_potion");
+		$niveau_courant = $this->request->get("niveau_courant");
+		
 		$typePotionCourante = null;
 
 		// On regarde si le hobbit est dans une de ses echopppes
@@ -18,6 +19,7 @@ class Bral_Competences_Elaborer extends Bral_Competences_Competence {
 			$this->view->elaborerEchoppeOk = false;
 			return;
 		}
+		
 		$idEchoppe = -1;
 		foreach($echoppes as $e) {
 			if ($e["id_fk_hobbit_echoppe"] == $this->view->user->id_hobbit &&
@@ -31,10 +33,7 @@ class Bral_Competences_Elaborer extends Bral_Competences_Competence {
 				'id_echoppe' => $e["id_echoppe"],
 				'x_echoppe' => $e["x_echoppe"],
 				'y_echoppe' => $e["y_echoppe"],
-				'id_metier' => $e["id_metier"],
-				'quantite_planche_arriere_echoppe' => $e["quantite_planche_arriere_echoppe"],
-				'quantite_fourrure_arriere_echoppe' => $e["quantite_fourrure_arriere_echoppe"],
-				'quantite_cuir_arriere_echoppe' => $e["quantite_cuir_arriere_echoppe"],
+				'id_metier' => $e["id_metier"]
 				);
 				break;
 			}
@@ -43,8 +42,102 @@ class Bral_Competences_Elaborer extends Bral_Competences_Competence {
 		if ($this->view->elaborerEchoppeOk == false) {
 			return;
 		}
+		
+		Zend_Loader::loadClass("TypePotion");
+		$typePotionTable = new TypePotion();
+		$typePotionRowset = $typePotionTable->fetchall(null, "nom_type_potion");
+		$typePotionRowset = $typePotionRowset->toArray();
+		$tabTypePotion = null;
+		foreach($typePotionRowset as $t) {
+			$selected = "";
+			if ($id_type_courant == $t["id_type_potion"]) {
+				$selected = "selected";
+			}
+			$t = array(
+			'id_type_potion' => $t["id_type_potion"],
+			'nom_type_potion' => $t["nom_type_potion"],
+			'selected' => $selected
+			);
+			if ($id_type_courant == $t["id_type_potion"]) {
+				$typePotionCourant = $t;
+			}
+			$tabTypePotion[] = $t;
+		}
+		
+		$tabNiveaux = null;
+		$tabCout = null;
+		$this->view->ressourcesOk = true;
+		$this->view->etape1 = false;
+		$this->view->typePotionCourant = null;
+		$this->view->cout = null;
+		$this->view->niveaux = null;
+		$this->view->elaborerPlanteOk = false;
 
-		$this->view->typePotion = $tabTypeEquipement;
+		if (isset($typePotionCourant)) {
+			Zend_Loader::loadClass("RecettePotions");
+			Zend_Loader::loadClass("EchoppePartieplante");
+
+			$this->view->etape1 = true;
+
+			for ($i = 0; $i <= $this->view->user->niveau_hobbit / 10 ; $i++) {
+				$tabNiveaux[$i] = array('niveauText' => 'Niveau '.$i, 'ressourcesOk' => true);
+			}
+			
+			$recettePotionsTable = new RecettePotions();
+			$recettePotions = $recettePotionsTable->findByIdTypePotion($typePotionCourant["id_type_potion"]);
+			
+			Zend_Loader::loadClass("EchoppePartiePlante");
+			$tabPartiePlantes = null;
+			$echoppePlanteTable = new EchoppePartiePlante();
+			$partiesPlantes = $echoppePlanteTable->findByIdEchoppe($idEchoppe);
+			
+			if ($partiesPlantes != null) {
+				foreach ($partiesPlantes as $m) {
+					if ($m["quantite_preparees_echoppe_partieplante"] > 1) {
+						$tabPartiePlantes[$m["id_fk_type_plante_echoppe_partieplante"]][$m["id_fk_type_echoppe_partieplante"]] = array(
+						"nom_type_partieplante" => $m["nom_type_partieplante"],
+						"nom_type" => $m["nom_type_plante"],
+						"quantite_preparees" => $m["quantite_preparees_echoppe_partieplante"],
+						);
+						$this->view->elaborerPlanteOk = true;
+					}
+				}
+			}
+			
+			foreach($tabNiveaux as $k => $v) {
+				foreach($recettePotions as $r) {
+					$tabCout[$k][] = array("nom_type_plante"=>$r["nom_type_plante"],"nom_type_partieplante"=>$r["nom_type_partieplante"], "cout" => ($r["coef_recette_potion"] + $k));
+					if ($r["coef_recette_potion"] + $k > $tabPartiePlantes[$r["id_fk_type_plante_recette_potion"]][$r["id_fk_type_partieplante_recette_potion"]]["quantite_preparees"]) {
+						$tabNiveaux[$k]["ressourcesOk"] = false;
+					}
+				}
+			}
+				
+					/*if ($r["niveau_recette_cout"] <= floor($this->view->user->niveau_hobbit / 10) ) {
+					if ($r["cuir_recette_cout"] > 0) {
+						$tabCout[$r["niveau_recette_cout"]][] = array("nom"=>"Cuir", "nom_systeme"=>"cuir", "cout" => $r["cuir_recette_cout"]);
+						if ($r["cuir_recette_cout"] > $echoppeCourante["quantite_cuir_arriere_echoppe"]) {
+							$tabNiveaux[$r["niveau_recette_cout"]]["ressourcesOk"] = false;
+						}
+					}
+					if ($r["fourrure_recette_cout"] > 0) {
+						$tabCout[$r["niveau_recette_cout"]][] = array("nom"=>"Fourrure", "nom_systeme"=>"fourrure", "cout" => $r["fourrure_recette_cout"]);
+						if ($r["fourrure_recette_cout"] > $echoppeCourante["quantite_fourrure_arriere_echoppe"]) {
+							$tabNiveaux[$r["niveau_recette_cout"]]["ressourcesOk"] = false;
+						}
+					}
+					if ($r["planche_recette_cout"] > 0) {
+						$tabCout[$r["niveau_recette_cout"]][] = array("nom"=>"Planche", "nom_systeme"=>"planche", "cout" => $r["planche_recette_cout"]);
+						if ($r["planche_recette_cout"] > $echoppeCourante["quantite_planche_arriere_echoppe"]) {
+							$tabNiveaux[$r["niveau_recette_cout"]]["ressourcesOk"] = false;
+						}
+					}*/
+			$this->view->cout = $tabCout;
+			$this->view->niveaux = $tabNiveaux;
+			$this->view->typePotionCourant = $typePotionCourant;
+		}
+
+		$this->view->typePotion = $tabTypePotion;
 		$this->idEchoppe = $idEchoppe;
 		$this->echoppeCourante = $echoppeCourante;
 	}
@@ -190,11 +283,12 @@ class Bral_Competences_Elaborer extends Bral_Competences_Competence {
 			'type_vente_echoppe_equipement' => 'aucune',
 			);
 			$echoppeEquipementTable->insert($data);
-*/
+
 		} else {
 			throw new Zend_Exception(get_class($this)." Recette inconnue: id=".$idTypePotion." n=".$niveau. " q=".$qualite);
 				
 		}
+*/
 	}
 
 	public function getIdEchoppeCourante() {
