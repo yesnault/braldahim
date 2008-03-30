0ed8a7751e66ef07dbdfc0afb6922096c9213031
BouleHondevice <boule.hondevice@braldahim.com>
2008-03-30 20:51:14 +0000

    Avancement
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@297 8885cbc4-9223-0410-882a-37b6fa6ce781

--- braldahim/library/Bral/Box/Communaute.php
+++ braldahim/library/Bral/Box/Communaute.php
@@ -21,7 +21,19 @@ class Bral_Box_Communaute {
 	}
 
 	function render() {
-	
+		Zend_Loader::loadClass("HobbitCommunaute");
+		
+		$tabCommunaute = null;
+		$hobbitCommunauteTable = new HobbitCommunaute();
+		$communauteRowset = $hobbitCommunauteTable->findByIdHobbit($this->view->user->id_hobbit);
+		
+		if (count($communauteRowset) > 0) {
+			foreach ($communauteRowset as $c) {
+				$tabCommunaute = $c["nom_communaute"];
+				break;
+			}
+		}
+		$this->view->communaute = $tabCommunaute;
 		$this->view->nom_interne = $this->getNomInterne();
 		return $this->view->render("interface/communaute.phtml");
 	}
