c05646ecbc7050e374ee495dca2cfc5c6f75e68d
BouleHondevice <boule.hondevice@braldahim.com>
2008-04-08 21:42:34 +0000

    Avancement
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@317 8885cbc4-9223-0410-882a-37b6fa6ce781

---
+++ braldahim/public/scripts/braldahim/braldahim_001/src/m_action.js
@@ -0,0 +1,210 @@
+function findSelectedRadioButton( groupname ) {
+	var radioButtons = $('myForm').elements[groupname];
+	for( var i = 0; i < radioButtons.length; i++ ) {
+		if( radioButtons[i].checked ) {
+			return radioButtons[i];
+		}
+	}
+	return null;
+}
+
+function _get_(url, encode){
+  var valeurs = "";
+  var nb_valeurs = 0;
+  var action = "";
+  
+  $("box_chargement").style.display = "block";
+  
+  if (url.length > 34) {
+  	if (url.substring(0, 12) == "/competences") { // /competences/doaction?caction=ask/do
+  		if ((url.substring(13, 15) == "do") && (url.substring(30, 32) == "do")) {
+  			action = "do";
+  		}
+   	} else if (url.substring(0, 9) == "/echoppes") { // /echoppes/doaction?caction=ask/do
+  		if ((url.substring(10, 12) == "do") && (url.substring(27, 29) == "do")) {
+  			action = "do";
+  		}
+  	} else if (url.substring(0, 6) == "/lieux") { // /lieux/doaction?caction=ask/do
+  		if ((url.substring(7, 9) == "do") && (url.substring(24, 26) == "do")) {
+  			action = "do";
+  		}
+  	} else if (url.substring(0, 11) == "/messagerie") { // /messagerie/doaction?caction=ask/do
+  		if ((url.substring(12, 14) == "do") && (url.substring(29, 31) == "do")) {
+  			action = "do";
+  		}
+	} else if (url.substring(0, 11) == "/communaute") { // /communaute/doaction?caction=ask/do
+  		if ((url.substring(12, 14) == "do") && (url.substring(29, 31) == "do")) {
+  			action = "do";
+  		}
+  	}
+  }
+
+  if ($('nb_valeurs') && (action == "do")) {
+      // Recuperation du nombre de valeur que l'action a besoin
+      nb_valeurs = $('nb_valeurs').value;
+     // alert('nb_valeurs='+nb_valeurs);
+      var sep = '';
+      for (i = 1; i<=nb_valeurs ; i++) {
+            var nom = 'valeur_'+i;
+            var elem = $(nom);
+            if (elem.type == "radio") {
+            	radioButton = findSelectedRadioButton(nom);
+            	if (radioButton != null) {
+                	valeurs = valeurs + sep + "valeur_" +i+ "=" +findSelectedRadioButton(nom).value;
+            	} else {
+            		valeurs = valeurs + sep + "valeur_" +i+ "=" +elem.value;
+            	}
+            } else {
+            	if (encode) {
+            		valeurs = valeurs + sep + "valeur_" +i+ "=" +encodeURIComponent(elem.value);
+            	} else {
+            		valeurs = valeurs + sep + "valeur_" +i+ "=" + elem.value;
+            	}
+               //valeurs = valeurs + "&" + $('valeur_'+i).serialize();
+            }
+            sep = "&";
+      }
+    //  alert('valeurs='+valeurs);
+  } else if ($('nb_valeurs') && (action == "ask")) {
+	Modalbox.hide();
+  }
+  
+  var pars = valeurs;
+  //alert("url="+url);
+ // alert("pars="+pars);
+  var myAjax = new Ajax.Request( url, {postBody: pars, onComplete: showResponse} );
+}
+
+function showResponse(originalRequest) {
+    var xmldoc = originalRequest.responseXML;
+    var textdoc = originalRequest.responseText;
+	var display_action = false;
+	var display_informations = false;
+	var display_erreur = false;
+	var activer_wysiwyg = false;
+	
+	var xmlHeader = '<?xml version="1.0" encoding="iso-8859-1" ?>';
+	
+    if ((xmldoc == null) || (textdoc.substring(0, 44) != xmlHeader)) {
+    	if (textdoc != "clear") {
+    		alert('Une erreur inconnue est survenue. Text:\n'+textdoc);
+    	}
+    } else {
+    	estInternetExplorer = false;
+		if (navigator.appName=="Microsoft Internet Explorer") {
+			estInternetExplorer = false;
+		} else {
+			estInternetExplorer = true;
+		}
+		
+	    var root = xmldoc.getElementsByTagName('root').item(0);
+	    for (var iNode = 0; iNode < root.childNodes.length; iNode++) {
+	      var node = root.childNodes.item(iNode);
+	
+	      for (i = 0; i < node.childNodes.length; i++) {
+	           var sibl = node.childNodes.item(i);
+	           if (estInternetExplorer == false) {
+				if (i == 0) {
+		              m_type = sibl.text
+				}
+		        if (i == 1) {
+		              m_type_valeur = sibl.text
+				}
+		        if (i == 2) {
+		            m_data = sibl.text
+				}
+	            if (i == 2) {
+				  //alert('Fin entrie \n m_type='+m_type+' \n m_type_valeur='+m_type_valeur);
+	              if (m_type_valeur == "box_action")
+	                display_action = true;
+	              else if (m_type_valeur == "informations" && m_data !="")
+	                display_informations = true; // affichage de la boite d'informations
+	              else if (m_type_valeur == "erreur" && m_data !="")
+	                display_erreur = true; // affichage de la boite d'erreur
+	                     
+	              if (m_type == "display") {
+	                _display_(m_type_valeur, m_data);
+	              } else if  (m_type == "action") {
+	              	if (m_type_valeur == "goto" && m_data !="") {
+	              		redirection = true;
+	              		redirection_url = m_data;
+	              	} else if (m_type_valeur == "effect.appear" && m_data !="") {
+	              		Effect.Appear(m_data, { duration: 2.0 });
+	              	}
+	              }
+	            }
+	           } else {
+	             for (x = 0; x < sibl.childNodes.length; x++) {
+	              if (i == 1)
+	                m_type = node.childNodes.item(1).childNodes.item(0).data;
+	              if (i == 3)
+	                m_type_valeur = node.childNodes.item(3).childNodes.item(0).data;
+	              if (i == 5)
+	                m_data = node.childNodes.item(5).childNodes.item(0).data;
+	              if (i == 5) {
+	            
+				   //alert('Fin entrie \n m_type='+m_type+' \n m_type_valeur='+m_type_valeur);
+	               if (m_type_valeur == "box_action")
+	                 display_action = true;
+	               else if (m_type_valeur == "informations" && m_data !="")
+	                 display_informations = true; // affichage de la boite d'informations
+	               else if (m_type_valeur == "erreur" && m_data !="")
+	                 display_erreur = true; // affichage de la boite d'erreur
+	                     
+	                if (m_type == "display") {
+	                  _display_(m_type_valeur, m_data);
+	                } else if  (m_type == "action") {
+	                	if (m_type_valeur == "goto" && m_data !="") {
+	              		redirection = true;
+	              		redirection_url = m_data;
+	                	} else if (m_type_valeur == "activer_wysiwyg" && m_data !="") {
+		              		tinyMCE.idCounter=0;
+		               		tinyMCE.execCommand('mceAddControl', false, m_data);
+		                } else if (m_type_valeur == "effect.disappear" && m_data !="") {
+		              		Effect.Appear(m_data, { duration: 4.0 , from:1.0, to:0.0});
+		              	}
+	                }
+	              }
+	            }	         
+	         }
+	      }
+	    }
+	}
+    // Box action
+	if (display_action) {
+		//$("box_action").style.display = "block";
+		Modalbox.show($("box_action"), {title: 'Action', width: 450, overlayClose:false});
+	} /*else {
+		if ($("box_action")) {
+			$("box_action").style.display = "none";
+		}
+	}*/
+
+    // Box informations
+	if (display_informations) {
+		//$("informations").style.display = "block";
+		Modalbox.show($("informations"), {title: 'Informations', width: 450, overlayClose:false});
+	} /*else {
+		if ($("informations")) {
+			$("informations").style.display = "none";
+		}
+	}*/
+    
+    // Box erreur
+	if (display_erreur) {
+		//$("erreur").style.display = "block";
+		Modalbox.show($("erreur"), {title: 'Une erreur est survenu', width: 400, overlayClose:false});
+	} else {
+		if ($("erreur")) {
+			$("erreur").style.display = "none";
+		}
+	}
+		
+    $("box_chargement").style.display = "none";
+    
+	if (redirection) {
+		document.location.href = redirection_url;
+	}
+    
+    return ;
+}
---
+++ braldahim/public/scripts/braldahim/braldahim_001/src/m_display.js
@@ -0,0 +1,151 @@
+
+function _display_(box,data) {
+	_display_box(box, data);
+}
+
+function _display_box(box, data) {
+	if ($(box)) {
+		$(box).innerHTML = data;
+	}
+	
+	if (box == 'racine') { // si l'on fait appel a boxes, on appel la vue ensuite
+		_get_('/interface/vue');
+	}
+}
+
+// Switch pour les onglets sur les box
+function my_switch(box,conteneur) {
+	val = $('switch_'+conteneur).value.split(',');
+	for (i=0; i<val.length; i++) {
+		if ($(val[i])) {
+			$(val[i]).style.display="none";
+		}
+		$("onglet_"+val[i]).className="onglet inactif";
+	}
+	if ($(box)) {
+		$(box).style.display="block";
+	}
+	$("onglet_"+box).className="onglet actif";
+}
+
+// Switch pour afficher un div et en cacher un autre
+function switch2div(div1, div2) {
+	if ($(div1).style.display=="none") {
+		$(div1).style.display="block";
+		$(div2).style.display="none";
+	} else {
+		$(div1).style.display="none";
+		$(div2).style.display="block";
+	}
+}
+
+// n'autorise que des chiffres.
+// exemple d'utilisation : <input type="text" onkeypress="chiffres(event)">
+function chiffres(event, negatif) {
+	// Compatibilité IE / Firefox
+	if(!event&&window.event) {
+		event=window.event;
+	}
+	
+	// IE 
+	if (event.keyCode == 37 || event.keyCode == 39 || // fleches deplacement
+		event.keyCode == 46 || event.keyCode == 8) { // backspace ou delete
+		return;
+	} else if(event.keyCode < 48 || event.keyCode > 57) {
+		event.returnValue = false;
+		event.cancelBubble = true;
+	}
+	
+	// DOM
+	if (event.which == 46 || event.which == 8) { // backspace ou delete
+		return;
+	} else if (negatif != null && event.which == 45) { // signe -
+		return;
+	} else if(event.which < 48 || event.which > 57) {
+		event.preventDefault();
+		event.stopPropagation();
+	}
+}
+
+function init_wisiwig() {
+	tinyMCE.init({
+		// General options
+		mode : "textareas",
+		theme : "advanced",
+		language : "fr",
+		plugins : "safari,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
+
+		// Theme options
+		theme_advanced_buttons1 : "newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,fontselect,fontsizeselect",
+		theme_advanced_buttons2 : "bullist,numlist,|,forecolor,backcolor,|,link,unlink,anchor,image,cleanup,emotions,",
+		theme_advanced_buttons3 : "",
+		theme_advanced_buttons4 : "",
+		theme_advanced_toolbar_location : "top",
+		theme_advanced_toolbar_align : "left",
+		theme_advanced_statusbar_location : "bottom",
+		theme_advanced_resizing : true,
+		theme_advanced_path : false,
+		relative_urls : false
+	});
+}
+function activer_wysiwyg(id) {
+	alert('Activation');
+	//tinyMCE.execCommand('mceAddControl', false, id);
+}
+
+function desactiver_wysiwyg(id) {
+	tinyMCE.execCommand('mceRemoveControl', false, id);
+}
+
+function accordion(el, ancien, visible) {
+	if ($(visible)) {
+		if ($(visible) == el) {
+		    return;
+		}
+	}
+	var eldown = el.parentNode.id+'-body';
+	var elup = null;
+	if ($(visible)) {
+		var elup = $(visible).parentNode.id+'-body';
+	}
+	if ($(elup) && $(eldown)) {
+		new Effect.Parallel(
+		[
+		    new Effect.SlideUp(elup),
+		    new Effect.SlideDown(eldown)
+		], {
+		    duration: 0.1
+		});
+	} else {
+		if ($(elup)) {
+			new Effect.SlideUp(elup, {duration: 0.1});
+		}
+		if ($(eldown)) {
+			new Effect.SlideDown(eldown, {duration: 0.1});
+		}
+	}
+	el.style.backgroundImage = "url(/public/images/expanded.gif);";
+	
+	if ($($(ancien).value+""+'-head')) {
+		$($(ancien).value+""+'-head').style.backgroundImage = "url(/public/images/collapsed.gif);";
+	}
+	if ($(visible)) {
+		$(visible).style.backgroundImage = "url(/public/images/collapsed.gif);";
+		$(visible).id = '';
+	}
+	$(ancien).value = el.parentNode.id;
+	el.id = visible;
+}
+
+function limiteTailleTextarea(textarea, max, iddesc) {
+    if(textarea.value.length >= max) {
+        textarea.value = textarea.value.substring(0,max);
+    }
+    var reste = max - textarea.value.length;
+    var affichage_reste =  reste + ' caract&egrave;res restants';
+    $(iddesc).innerHTML = affichage_reste;
+}
+
+function ouvrirWin(url, titre) {
+	window.open(url,titre, "directories=no,location=no,menubar=no,resizable=yes,scrollbars=yes,status=no,toolbar=no,width=600,height=600");	
+}
