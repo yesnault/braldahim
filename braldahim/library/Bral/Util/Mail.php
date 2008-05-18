42ed362d8b571c0a047e6a1c604245e8727d7f98
BouleHondevice <boule.hondevice@braldahim.com>
2008-05-18 22:32:54 +0000

    Avancement
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@369 8885cbc4-9223-0410-882a-37b6fa6ce781

--- braldahim/library/Bral/Util/Mail.php
+++ braldahim/library/Bral/Util/Mail.php
@@ -3,10 +3,10 @@
 class Bral_Util_Mail {
 	public static function getNewZendMail() {
 		Zend_Loader::loadClass("Zend_Mail");
-		Zend_Loader::loadClass("Zend_Mail_Transport_Smtp");
-		$c = Zend_Registry::get('config');
-		$transport = new Zend_Mail_Transport_Smtp($c->general->mail->smtp_server);
-		Zend_Mail::setDefaultTransport($transport);
+//		Zend_Loader::loadClass("Zend_Mail_Transport_Smtp");
+//		$c = Zend_Registry::get('config');
+//		$transport = new Zend_Mail_Transport_Smtp($c->general->mail->smtp_server);
+//		Zend_Mail::setDefaultTransport($transport);
 		return new Zend_Mail();
 	}
 }
