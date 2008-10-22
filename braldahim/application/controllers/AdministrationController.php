8fd3fd0a5dcbbd08c32da332633d5ed9f499e661
BouleHondevice <boule.hondevice@braldahim.com>
2008-10-22 22:20:00 +0000

    Entête
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@564 8885cbc4-9223-0410-882a-37b6fa6ce781

--- braldahim/application/controllers/AdministrationController.php
+++ braldahim/application/controllers/AdministrationController.php
@@ -1,13 +1,14 @@
 <?php
 
 /**
+ * This file is part of Braldahim, under Gnu Public Licence v3. 
+ * See licence.txt or http://www.gnu.org/licenses/gpl-3.0.html
+ *
  * $Id$
+ * $Author$
  * $LastChangedDate$
  * $LastChangedRevision$
- * $Revision$
  * $LastChangedBy$
- * $Author$
- * $Date$
  */
 class AdministrationController extends Zend_Controller_Action {
 	
