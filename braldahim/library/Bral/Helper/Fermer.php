5b61d6863d978d38b941cf2636f2227c2444b313
BouleHondevice <boule.hondevice@braldahim.com>
2008-11-10 13:43:55 +0000

    Bouton fermer
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@601 8885cbc4-9223-0410-882a-37b6fa6ce781

---
+++ braldahim/library/Bral/Helper/Fermer.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * This file is part of Braldahim, under Gnu Public Licence v3. 
+ * See licence.txt or http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * $Id:$
+ * $Author:$
+ * $LastChangedDate:$
+ * $LastChangedRevision:$
+ * $LastChangedBy:$
+ */
+class Bral_Helper_Fermer {
+
+	public static function affiche() {
+		return "<input type='button' class='button1' value='Fermer' onClick='this.disabled=true;_get_(\"/interface/clear\");Modalbox.hide();'>";
+	}
+}
+
