21e8d4d2d0651d3d3a2c6abcaecdb9d9af5dece5
PulgMaltertre <pulg.maltertre@braldahim.com>
2008-12-04 21:07:22 +0000

    Correction 205
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@680 8885cbc4-9223-0410-882a-37b6fa6ce781

--- braldahim/library/Bral/Util/Connaissance.php
+++ braldahim/library/Bral/Util/Connaissance.php
@@ -3,11 +3,11 @@
  * This file is part of Braldahim, under Gnu Public Licence v3. 
  * See licence.txt or http://www.gnu.org/licenses/gpl-3.0.html
  *
- * $Id:$
- * $Author:$
- * $LastChangedDate:$
- * $LastChangedRevision:$
- * $LastChangedBy:$
+ * $Id$
+ * $Author$
+ * $LastChangedDate$
+ * $LastChangedRevision$
+ * $LastChangedBy$
  */
 class Bral_Util_Connaissance {
 	
@@ -27,4 +27,3 @@ class Bral_Util_Connaissance {
 	}
 	
 }
-?>
