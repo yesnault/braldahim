36c684977fbf0e1d67130aba2fbad6ca1ea651ac
PulgMaltertre <pulg.maltertre@braldahim.com>
2008-12-14 22:27:06 +0000

    avancement recyclage
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@745 8885cbc4-9223-0410-882a-37b6fa6ce781

--- braldahim/library/Bral/Competences/Recyclage.php
+++ braldahim/library/Bral/Competences/Recyclage.php
@@ -98,31 +98,77 @@ class Bral_Competences_Recyclage extends Bral_Competences_Competence {
 		
 		$recetteCoutTable = new RecetteCout();
 		$recetteCout = $recetteCoutTable->findByIdTypeEquipementAndNiveau($idTypeEquipement, $nivEquipement);
-
+		
+		/*
+		 * Soit Ni le niveau de la pièce d'équipement. Soit Js un jet de SAG + BM.
+		 * Si Js < Ni*10 alors 25% de chacun des composants arrondis à l'inférieur sont récupérés
+		 * Si Ni*10 < Js < Ni*20 alors 50 % des ...
+		 * Si Ni*20 < Js < Ni*30 alors 60 % des ...
+		 * Si Ni*30 < Js  alors 80 % des ...
+		 */
+		
+		$jetSag = 0;
+		for ($i=1; $i<=$this->view->config->game->base_sagesse + $this->view->user->sagesse_base_hobbit; $i++) {
+			$jetSag = $jetSag + Bral_Util_De::get_1d6();
+		}
+		$jetSag = $jetSag + $this->view->user->sagesse_bm_hobbit + $this->view->user->sagesse_bbdf_hobbit;
+		
+		if ($jetSag < $nivEquipement*10) {
+			$perte = 0.25;
+		}
+		elseif ($jetSag > $nivEquipement*10 && $jetSag < $nivEquipement*20) {
+			$perte = 0.5;
+		}
+		elseif ($jetSag > $nivEquipement*20 && $jetSag < $nivEquipement*30) {
+			$perte = 0.6;
+		}
+		elseif ($jetSag > $nivEquipement*30) {
+			$perte = 0.8;
+		}
+		
 		foreach($recetteCout as $r) {
-			$nbCuir = $r["cuir_recette_cout"];
-			$nbFourrure = $r["fourrure_recette_cout"];
-			$nbPlanche = $r["planche_recette_cout"];
+			$nbCuir = floor($r["cuir_recette_cout"]*$perte);
+			$nbFourrure = floor($r["fourrure_recette_cout"]*$perte);
+			$nbPlanche = floor($r["planche_recette_cout"]*$perte);
 		}
 		
 		$recetteCoutMineraiTable = new RecetteCoutMinerai();
 		$recetteCoutMinerai = $recetteCoutMineraiTable->findByIdTypeEquipementAndNiveau($idTypeEquipement, $nivEquipement);
-		
-		foreach($recetteCoutMinerai as $r){
-			$tabMinerai[] = array (
-				"nom" => $r["nom_type_minerai"],
-				"quantite" => $r["quantite_recette_cout_minerai"],
-			);
+		if (count ($recetteCoutMinerai) > 0) {
+			Zend_Loader::loadClass("LabanMinerai");
+			$labanMineraiTable = new LabanMinerai();
+			foreach($recetteCoutMinerai as $r){
+				$quantite = floor($r["quantite_recette_cout_minerai"]*$perte);
+				$tabMinerai[] = array (
+					"nom" => $r["nom_type_minerai"],
+					"quantite" => $quantite,
+				);
+				$data = array(
+					'id_fk_type_laban_minerai' => $r["id_type_minerai"],
+					'id_fk_hobbit_laban_minerai' => $this->view->user->id_hobbit,
+					'quantite_brut_laban_minerai' => $quantite,
+				);
+				$labanMineraiTable->insertOrUpdate($data);
+			}
 		}
-		
 		/*
 		 * TODO
-		 * Calculer le nombre de matière à garder
-		 * Mettre à jour le laban avec les matières
 		 * Controler le poids
 		 * Supprimer l'equipement
 		 * Afficher le résultat
+		 * Runes
+		 * tests avec minerai
 		 */
+		
+		// on ajoute dans le laban
+		$labanTable = new Laban();
+		$data = array(
+			'id_fk_hobbit_laban' => $this->view->user->id_hobbit,
+			'quantite_cuir_laban' => $nbCuir,
+			'quantite_fourrure_laban' => $nbFourrure,
+			'quantite_planche_laban' => $nbPlanche,
+		);
+		$labanTable->insertOrUpdate($data);
 		
 		$this->view->nbCuir = $nbCuir;
 		$this->view->nbFourrure = $nbFourrure;
