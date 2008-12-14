1af9503de0fb1f977ec5d8a2109aee0e1baf2316
BouleHondevice <boule.hondevice@braldahim.com>
2008-12-14 22:54:27 +0000

    Correction #58
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@749 8885cbc4-9223-0410-882a-37b6fa6ce781

--- braldahim/library/Bral/Util/Attaque.php
+++ braldahim/library/Bral/Util/Attaque.php
@@ -310,6 +310,7 @@ class Bral_Util_Attaque {
 				$gainPv = ($retourAttaque["jetDegat"] / 2);
 				if ($gainPv > $effetMotE * 3) {
 					$gainPv = $effetMotE * 3;
+					$retourAttaque["effetMotEPoints"] = $gainPv;
 				}
 				Bral_Util_Log::attaque()->debug("Bral_Util_Attaque - effetMotE True effetMotE=".$effetMotE." gainPv=".$gainPv);
 				
