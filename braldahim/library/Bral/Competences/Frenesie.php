899fed4e6c8f79680d0f548bf6da41d341a3f80f
BouleHondevice <boule.hondevice@braldahim.com>
2008-12-19 22:08:35 +0000

    Correction jet Attaque
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@786 8885cbc4-9223-0410-882a-37b6fa6ce781

--- braldahim/library/Bral/Competences/Frenesie.php
+++ braldahim/library/Bral/Competences/Frenesie.php
@@ -141,7 +141,10 @@ class Bral_Competences_Frenesie extends Bral_Competences_Competence {
 		for ($i=1; $i<=$this->view->config->game->base_agilite + $hobbit->agilite_base_hobbit; $i++) {
 			$jetAttaquant = $jetAttaquant + Bral_Util_De::get_1d6();
 		}
-		$jetAttaquant = (0.5 * $jetAttaquant) + $hobbit->agilite_bm_hobbit + $hobbit->agilite_bbdf_hobbit + $hobbit->bm_attaque_hobbit;
+		$jetAttaquant = floor((0.5 * $jetAttaquant) + $hobbit->agilite_bm_hobbit + $hobbit->agilite_bbdf_hobbit + $hobbit->bm_attaque_hobbit);
+		if ($jetAttaquant < 0) {
+			$jetAttaquant = 0;
+		}
 		return $jetAttaquant;
 	}
 
