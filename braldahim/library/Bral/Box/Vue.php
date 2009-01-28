cb272b5815cdf90780466f79e33d8dd265b4529d
BouleHondevice <boule.hondevice@braldahim.com>
2009-01-28 19:33:12 +0000

    Ajout icone Olivier
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@1095 8885cbc4-9223-0410-882a-37b6fa6ce781

--- braldahim/library/Bral/Box/Vue.php
+++ braldahim/library/Bral/Box/Vue.php
@@ -308,7 +308,7 @@ class Bral_Box_Vue extends Bral_Box_Box {
 								} else {
 									$nom_metier = $e["nom_masculin_metier"];
 								}
-								$tabEchoppes[] = array("id_echoppe" => $e["id_echoppe"], "nom_echoppe" => $e["nom_echoppe"], "nom_metier" => $nom_metier, "nom_hobbit" => $e["nom_hobbit"], "prenom_hobbit" => $e["prenom_hobbit"], "id_hobbit" => $e["id_hobbit"]);
+								$tabEchoppes[] = array("id_echoppe" => $e["id_echoppe"], "nom_echoppe" => $e["nom_echoppe"], "nom_systeme_metier"=> $e["nom_systeme_metier"], "nom_metier" => $nom_metier, "nom_hobbit" => $e["nom_hobbit"], "prenom_hobbit" => $e["prenom_hobbit"], "id_hobbit" => $e["id_hobbit"]);
 							}
 						}
 					}
