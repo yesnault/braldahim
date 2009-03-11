dc4f507639e36d3113fd6562362628541941b166
BouleHondevice <boule.hondevice@braldahim.com>
2009-03-11 11:49:56 +0000

    lien infoJeu
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@1300 8885cbc4-9223-0410-882a-37b6fa6ce781

--- braldahim-site/application/controllers/IndexController.php
+++ braldahim-site/application/controllers/IndexController.php
@@ -34,6 +34,7 @@ class IndexController extends Zend_Controller_Action {
 				"date_info_jeu" => $i["date_info_jeu"],
 				"text_info_jeu" => $i["text_info_jeu"],
 				"est_sur_accueil_info_jeu" => $i["est_sur_accueil_info_jeu"],
+				"lien_info_jeu" => $i["lien_info_jeu"],
 				);
 		}
 		
