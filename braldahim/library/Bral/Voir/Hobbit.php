34068c4e86826a1cd91fab43d3cc569c2108cb19
BouleHondevice <boule.hondevice@braldahim.com>
2009-04-18 10:47:29 +0000

    Correction exception
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@1443 8885cbc4-9223-0410-882a-37b6fa6ce781

--- braldahim/library/Bral/Voir/Hobbit.php
+++ braldahim/library/Bral/Voir/Hobbit.php
@@ -212,7 +212,7 @@ class Bral_Voir_Hobbit {
 
 		// on va chercher les informations du conjoint
 		Zend_Loader::loadClass("Bral_Util_Conjoints");
-		$this->view->conjoint = Bral_Util_Conjoints::getConjoint($this->view->user->sexe_hobbit, $this->view->user->id_hobbit);
+		$this->view->conjoint = Bral_Util_Conjoints::getConjoint($this->view->hobbit["sexe_hobbit"], $this->view->hobbit["id_hobbit"]);
 
 		$this->view->dateNaissance = Bral_Util_ConvertDate::get_datetime_mysql_datetime('d/m/y \&\a\g\r\a\v\e; H:i:s',$this->view->hobbit["date_creation_hobbit"]);
 		return $this->view->render("voir/hobbit/famille.phtml");
