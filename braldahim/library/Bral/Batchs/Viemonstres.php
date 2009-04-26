af9047a4ca0ea8fcf5fb8ae62759c0f0644b4347
BouleHondevice <boule.hondevice@braldahim.com>
2009-04-26 18:31:40 +0000

    Correction #372
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@1493 8885cbc4-9223-0410-882a-37b6fa6ce781

--- braldahim/library/Bral/Batchs/Viemonstres.php
+++ braldahim/library/Bral/Batchs/Viemonstres.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This file is part of Braldahim, under Gnu Public Licence v3. 
+ * This file is part of Braldahim, under Gnu Public Licence v3.
  * See licence.txt or http://www.gnu.org/licenses/gpl-3.0.html
  *
  * $Id$
@@ -11,39 +11,39 @@
  * $LastChangedBy$
  */
 class Bral_Batchs_Viemonstres extends Bral_Batchs_Batch {
-	
+
 	public function calculBatchImpl() {
 		Bral_Util_Log::batchs()->trace("Bral_Batchs_Viemonstres - calculBatchImpl - enter -");
 		$retour = null;
 
-        Zend_Loader::loadClass('GroupeMonstre');
-        Zend_Loader::loadClass('Monstre');
-        Zend_Loader::loadClass("Bral_Monstres_VieGroupes");
-        Zend_Loader::loadClass("Bral_Monstres_VieGroupesNuee");
-        Zend_Loader::loadClass("Bral_Monstres_VieMonstre");
-        Zend_Loader::loadClass("Bral_Monstres_VieSolitaire");
-        Zend_Loader::loadClass("Bral_Monstres_VieGibier");
-        Zend_Loader::loadClass("Bral_Util_Evenement");
-        Zend_Loader::loadClass("Bral_Util_Attaque");
-        Zend_Loader::loadClass("Bral_Util_Vie");
-        Zend_Loader::loadClass("Ville");
+		Zend_Loader::loadClass('GroupeMonstre');
+		Zend_Loader::loadClass('Monstre');
+		Zend_Loader::loadClass("Bral_Monstres_VieGroupes");
+		Zend_Loader::loadClass("Bral_Monstres_VieGroupesNuee");
+		Zend_Loader::loadClass("Bral_Monstres_VieMonstre");
+		Zend_Loader::loadClass("Bral_Monstres_VieSolitaire");
+		Zend_Loader::loadClass("Bral_Monstres_VieGibier");
+		Zend_Loader::loadClass("Bral_Util_Evenement");
+		Zend_Loader::loadClass("Bral_Util_Attaque");
+		Zend_Loader::loadClass("Bral_Util_Vie");
+		Zend_Loader::loadClass("Ville");
 
-        $villeTable = new Ville();
+		$villeTable = new Ville();
 		$villes = $villeTable->fetchAll();
-		
-/*		$vieGroupe = new Bral_Monstres_VieGroupesNuee($this->view, $villes);
-       	$vieGroupe->action();
-        
-        $vieSolitaire = new Bral_Monstres_VieSolitaire($this->view, $villes);
-        $vieSolitaire->action();
-  */      
-        $vieGibier = new Bral_Monstres_VieGibier($this->view, $villes);
-        $vieGibier->action();
-		
+
+		$vieGroupe = new Bral_Monstres_VieGroupesNuee($this->view, $villes);
+		$vieGroupe->action();
+
+		$vieSolitaire = new Bral_Monstres_VieSolitaire($this->view, $villes);
+		$vieSolitaire->action();
+
+		$vieGibier = new Bral_Monstres_VieGibier($this->view, $villes);
+		$vieGibier->action();
+
 		Bral_Util_Log::batchs()->trace("Bral_Batchs_Viemonstres - calculBatchImpl - exit -");
 		return $retour;
 	}
-	
+
 	private function purgeBatch() {
 	}
 }
