1cce8e6fa5c5f9447dbc9f938343aa70c1347e15
BouleHondevice <boule.hondevice@braldahim.com>
2009-04-27 06:53:11 +0000

    Correction "id_fk_hobbit_element_aliment" => $this->view->user->id_hobbit,
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@1498 8885cbc4-9223-0410-882a-37b6fa6ce781

--- braldahim/library/Bral/Competences/Cuisiner.php
+++ braldahim/library/Bral/Competences/Cuisiner.php
@@ -146,7 +146,6 @@ class Bral_Competences_Cuisiner extends Bral_Competences_Competence {
 				"id_fk_type_element_aliment" => TypeAliment::ID_TYPE_RAGOUT,
 				"x_element_aliment" => $this->view->user->x_hobbit,
 				"y_element_aliment" => $this->view->user->y_hobbit,
-				"id_fk_hobbit_element_aliment" => $this->view->user->id_hobbit,
 				"id_fk_type_qualite_element_aliment" => $this->view->qualiteAliment,
 				"bbdf_element_aliment" => $this->view->bbdfAliment,
 			);
