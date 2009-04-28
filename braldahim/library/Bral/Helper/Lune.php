67de5e64c4368cee5f05a6ee3bc2fa1ab4871669
BouleHondevice <boule.hondevice@braldahim.com>
2009-04-28 19:12:47 +0000

    Correction Lune
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@1503 8885cbc4-9223-0410-882a-37b6fa6ce781

--- braldahim/library/Bral/Helper/Lune.php
+++ braldahim/library/Bral/Helper/Lune.php
@@ -34,22 +34,29 @@ class Bral_Helper_Lune {
 	 echo "La Lune est éclairée $mpfrac à ".number_format($MoonPhase*100, 2, ',', '')."%"." nouvelle <br>";
 
 	 */
-
+	
+	public static function afficheSuivantDate($annee, $mois, $jour, $heure, $mine, $seconde) {
+		return  self::afficheImg($annee, $mois, $jour, $heure, $mine, $seconde);
+	}
+	
 	public static function affiche() {
-		Zend_Loader :: loadClass("Bral_Util_Lune");
-			
-			
 		$annee = date('Y');
 		$mois = date('m');
 		$jour = date('d');
 		$heure = date('H');
 		$mine = date('i');
 		$seconde = date('s');
+		
+		return  self::afficheImg($annee, $mois, $jour, $heure, $mine, $seconde);
+	} 
+
+	private static function afficheImg($annee, $mois, $jour, $heure, $mine, $seconde) {
+		Zend_Loader :: loadClass("Bral_Util_Lune");
 
 		list($moonPhase, $moonAge, $moonDist, $moonAng, $sunDist, $sunAng, $mpfrac) = Bral_Util_Lune::calculPhase($annee, $mois, $jour, $heure, $mine, $seconde);
 
 		$titre = "La Lune au ";
-		$titre .= Bral_Helper_Calendrier::affiche(true);
+		$titre .= Bral_Helper_Calendrier::affiche(true, $annee."-".$mois."-".$jour." ".$heure.":".$mine.":".$seconde);
 		
 		$age = floor($moonAge);
 		$s = '';
@@ -58,21 +65,21 @@ class Bral_Helper_Lune {
 		}
 
 		$libelle = '';
-		if ($mpfrac >= 99 || $mpfrac <= 1) {
+		if ($mpfrac >= 0.99 || $mpfrac <= 0.1) {
 			$libelle = 'une Nouvelle Lune';
-		} elseif ($mpfrac >= 24 && $mpfrac <= 26) { // premier quartier
+		} elseif ($mpfrac > 0.1 && $mpfrac < 0.24) { // premier quartier
 			$libelle = 'une Lune croissante';
-		} elseif ($mpfrac >= 24 && $mpfrac <= 26) { // premier quartier
+		} elseif ($mpfrac >= 0.24 && $mpfrac <= 0.26) { // premier quartier
 			$libelle = 'le Premier quartier';
-		} elseif ($mpfrac > 26 && $mpfrac < 49) {
+		} elseif ($mpfrac > 0.26 && $mpfrac < 0.49) {
 			$libelle = 'une Lune gibbeuse croissante';
-		} elseif ($mpfrac >= 49 && $mpfrac <= 51) { // pleine lune
+		} elseif ($mpfrac >= 0.49 && $mpfrac <= 0.51) { // pleine lune
 			$libelle = 'une Pleine Lune';
-		} elseif ($mpfrac > 51 && $mpfrac < 74) { 
+		} elseif ($mpfrac > 0.51 && $mpfrac < 0.74) { 
 			$libelle = 'une Lune gibbeuse décroissante';
-		} elseif ($mpfrac >= 74 && $mpfrac <= 76) { // Dernier quartier
+		} elseif ($mpfrac >= 0.74 && $mpfrac <= 0.76) { // Dernier quartier
 			$libelle = 'le Dernier quartier';
-		} elseif ($mpfrac > 76 && $mpfrac < 99) { // pleine lune
+		} elseif ($mpfrac > 0.76 && $mpfrac < 0.99) { // pleine lune
 			$libelle = 'une Lune décroissante';
 		}
 		
