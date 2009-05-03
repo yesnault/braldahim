426285387b019a9f71a3f9464f96d5e0b4ac7f3a
BouleHondevice <boule.hondevice@braldahim.com>
2009-05-03 18:39:15 +0000

    Correction #371
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@1514 8885cbc4-9223-0410-882a-37b6fa6ce781

---
+++ braldahim/library/Bral/Competences/Recherchermot.php
@@ -0,0 +1,220 @@
+<?php
+
+/**
+ * This file is part of Braldahim, under Gnu Public Licence v3.
+ * See licence.txt or http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * $Id: $
+ * $Author: $
+ * $LastChangedDate: $
+ * $LastChangedRevision: $
+ * $LastChangedBy: $
+ */
+class Bral_Competences_Recherchermot extends Bral_Competences_Competence {
+
+	function prepareCommun() {
+		Zend_Loader::loadClass('LabanRune');
+		// on verifie que le hobbit possede au moins une rune
+
+		$this->view->rechercherMotOk = false;
+
+		Zend_Loader::loadClass("MotRunique");
+		$motRuniqueTable = new MotRunique();
+		$motsRuniques = $motRuniqueTable->fetchAll(null, "suffixe_mot_runique ASC");
+
+		$tabMotsRuniques = null;
+		$longueurMax = floor($this->view->user->sagesse_base_hobbit / 2);
+
+		$motCourant = null;
+		$id_mot_courant = (int)$this->request->get("id_mot");
+
+		foreach($motsRuniques as $mot) {
+			$longueur = $this->calculLongeurMot($mot);
+			if ($this->calculLongeurMot($mot) <= $longueurMax) {
+				$selected = "";
+				if ($id_mot_courant == $mot["id_mot_runique"]) {
+					$selected = "selected";
+				}
+
+				$tabMotsRuniques[$mot["id_mot_runique"]] = array(
+					"id_mot_runique" => $mot["id_mot_runique"],
+					"suffixe_mot_runique" => $mot["suffixe_mot_runique"],
+					"id_fk_type_rune_1_mot_runique" => $mot["id_fk_type_rune_1_mot_runique"],
+					"id_fk_type_rune_2_mot_runique" => $mot["id_fk_type_rune_2_mot_runique"],
+					"id_fk_type_rune_3_mot_runique" => $mot["id_fk_type_rune_3_mot_runique"],
+					"id_fk_type_rune_4_mot_runique" => $mot["id_fk_type_rune_4_mot_runique"],
+					"id_fk_type_rune_5_mot_runique" => $mot["id_fk_type_rune_5_mot_runique"],
+					"id_fk_type_rune_6_mot_runique" => $mot["id_fk_type_rune_6_mot_runique"],
+					"nb_runes" => $longueur,
+					"selected" => $selected,
+				);
+
+				if ($id_mot_courant ==  $mot["id_mot_runique"]) {
+					$motCourant = $tabMotsRuniques[$mot["id_mot_runique"]];
+				}
+			}
+		}
+
+		Zend_Loader::loadClass("LabanRune");
+		$tabLabanRune = null;
+		$labanRuneTable = new LabanRune();
+		$labanRunes = $labanRuneTable->findByIdHobbit($this->view->user->id_hobbit, "oui");
+			
+		foreach($labanRunes as $l) {
+			$tabLabanRune[$l["id_rune_laban_rune"]] = array(
+					"id_fk_type_rune_laban_rune" => $l["id_fk_type_laban_rune"],
+					"nom_type_rune" => $l["nom_type_rune"],
+					"image_type_rune" => $l["image_type_rune"],
+					"effet_type_rune" => $l["effet_type_rune"],
+					"id_rune_laban_rune" => $l["id_rune_laban_rune"],
+			);
+			$this->view->rechercherMotOk = true;
+		}
+		$this->view->nbLabanRune = count($tabLabanRune);
+		$this->view->labanRunes = $tabLabanRune;
+		$this->view->motsRuniques = $tabMotsRuniques;
+		$this->view->motCourant = $motCourant;
+	}
+
+	private function calculLongeurMot($mot) {
+		$longueur = 100;
+		if ($mot["id_fk_type_rune_1_mot_runique"] != null &&
+		$mot["id_fk_type_rune_2_mot_runique"] != null &&
+		$mot["id_fk_type_rune_3_mot_runique"] != null &&
+		$mot["id_fk_type_rune_4_mot_runique"] != null &&
+		$mot["id_fk_type_rune_5_mot_runique"] != null &&
+		$mot["id_fk_type_rune_6_mot_runique"] != null) {
+			$longueur = 6;
+		} else if ($mot["id_fk_type_rune_5_mot_runique"] != null) {
+			$longueur = 5;
+		} else if ($mot["id_fk_type_rune_4_mot_runique"] != null) {
+			$longueur = 4;
+		} else if ($mot["id_fk_type_rune_3_mot_runique"] != null) {
+			$longueur = 3;
+		} else if ($mot["id_fk_type_rune_2_mot_runique"] != null) {
+			$longueur = 2;
+		} else {
+			$longueur = 1;
+		}
+		return $longueur;
+	}
+
+	function prepareFormulaire() {
+		if ($this->view->assezDePa == false) {
+			return;
+		}
+	}
+
+	function prepareResultat() {
+		if ($this->view->assezDePa == false) {
+			throw new Zend_Exception(get_class($this)." Pas assez de PA : ".$this->view->user->pa_hobbit);
+		}
+
+		if ($this->view->rechercherMotOk == false) {
+			throw new Zend_Exception(get_class($this)." Identifier Rune : pas de rune");
+		}
+
+		$idMot = $this->request->get("valeur_1");
+		$nbRunes = $this->request->get("valeur_2");
+		$runes = $this->request->get("valeur_3");
+
+		if ((int) $idMot."" != $this->request->get("valeur_1")."") {
+			throw new Zend_Exception(get_class($this)." Mot invalide=".$idMot);
+		} else {
+			$idMot = (int)$idMot;
+		}
+
+		if (!array_key_exists($idMot, $this->view->motsRuniques)) {
+			throw new Zend_Exception(get_class($this)." $idMot interdit A=".$idMot);
+		}
+
+		if ($this->view->motCourant["id_mot_runique"] != $idMot) {
+			throw new Zend_Exception(get_class($this)." mot courant invalide:a:".$this->view->motCourant["id_mot_runique"]. "m:".$idMot);
+		}
+		
+		$motRecherche = $this->view->motsRuniques[$idMot];
+
+		if ((int) $nbRunes."" != $this->request->get("valeur_2")."") {
+			throw new Zend_Exception(get_class($this)." Nb Rune invalide=".$nbRunes);
+		} else {
+			$nbRunes = (int)$nbRunes;
+		}
+
+		if ($runes == "" || $runes == null) {
+			throw new Zend_Exception(get_class($this)." Runes invalides=".$runes);
+		}
+
+		$tabRunesJs = explode(",", $runes);
+		$tabRunes = null;
+
+		// on regarde si les runes sont présentes dans le laban
+		$tmp = $this->view->labanRunes;
+		$nb = 0;
+		foreach($tabRunesJs as $u) {
+			$trouve = false;
+			foreach($tmp as $k => $r) {
+				if ((int)$u == $k) {
+					$nb++;
+					$tabRunes[$nb] = $r;
+					$trouve = true;
+					break;
+				}
+			}
+			if ($trouve == false) {
+				throw new Zend_Exception(get_class($this)." Rune invalide =".$u);
+			}
+		}
+
+		if ($nb != $nbRunes) {
+			throw new Zend_Exception(get_class($this)." Nombre de runes invalides A n1=".$nb. " n2=".$nbRunes);
+		}
+
+		if ($nb != $motRecherche["nb_runes"]) {
+			throw new Zend_Exception(get_class($this)." Nombre de runes invalides B n1=".$nb. " n2=".$motRecherche["nb_runes"]);
+		}
+		
+		// calcul des jets
+		$this->calculJets();
+
+		if ($this->view->okJet1 === true) {
+			$this->calculRecherchermot($motRecherche, $tabRunes);
+		}
+
+		$this->calculPx();
+		$this->calculBalanceFaim();
+		$this->majHobbit();
+	}
+
+	private function calculRecherchermot($motRecherche, $tabRunes) {
+		
+		$ordre = 0;
+		$nbPositionOk = 0;
+		$nbOkMalPlace = 0;
+		foreach($tabRunes as $k => $v) {
+			$ordre++;
+			if ($motRecherche["id_fk_type_rune_".$ordre."_mot_runique"] == null) {
+				break;
+			}
+			
+			if ($motRecherche["id_fk_type_rune_".$ordre."_mot_runique"] == $v["id_fk_type_rune_laban_rune"]) {
+				$nbPositionOk++;
+			} else {
+				for($i = 1; $i<=6; $i++) {
+					if ($motRecherche["id_fk_type_rune_".$i."_mot_runique"] != null && $motRecherche["id_fk_type_rune_".$i."_mot_runique"] == $v["id_fk_type_rune_laban_rune"]) {
+						$nbOkMalPlace++;
+					}
+				}
+			}
+		}
+		
+		$this->view->nbPositionOk = $nbPositionOk;
+		$this->view->nbOkMalPlace = $nbOkMalPlace;
+		
+		$this->view->tabRunes = $tabRunes;
+		$this->view->nbRunes = count($this->view->tabRunes);
+	}
+
+	function getListBoxRefresh() {
+		return $this->constructListBoxRefresh(array("box_competences_communes"));
+	}
+}
--- braldahim/library/Bral/Lieux/Joaillier.php
+++ braldahim/library/Bral/Lieux/Joaillier.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This file is part of Braldahim, under Gnu Public Licence v3. 
+ * This file is part of Braldahim, under Gnu Public Licence v3.
  * See licence.txt or http://www.gnu.org/licenses/gpl-3.0.html
  *
  * $Id$
@@ -17,19 +17,19 @@ class Bral_Lieux_Joaillier extends Bral_Lieux_Lieu {
 	private $_tabCompetences = null;
 
 	function prepareCommun() {
-		
+
 		$this->_coutCastars = $this->calculCoutCastars();
 		$this->view->coutCastars = $this->_coutCastars;
 		$this->view->achatPossibleCastars = ($this->view->user->castars_hobbit - $this->_coutCastars >= 0);
 		// $this->view->utilisationPaPossible initialisé dans Bral_Lieux_Lieu
-		
+
 		if ($this->view->utilisationPaPossible == false) {
 			return;
 		}
-		
+
 		Zend_Loader::loadClass("LabanEquipement");
 		Zend_Loader::loadClass("MotRunique");
-		
+
 		$this->view->effetMotF = false;
 
 		$id_equipement_courant = $this->request->get("id_equipement");
@@ -39,14 +39,14 @@ class Bral_Lieux_Joaillier extends Bral_Lieux_Lieu {
 		$equipements = $labanEquipementTable->findByIdHobbit($this->view->user->id_hobbit);
 
 		Zend_Loader::loadClass("Bral_Util_Equipement");
-		
+
 		if (count($equipements) > 0) {
 			foreach($equipements as $e) {
 				$selected = "";
 				if ($id_equipement_courant == $e["id_laban_equipement"]) {
 					$selected = "selected";
 				}
-			
+					
 				$t = array(
 					"id_laban_equipement" => $e["id_laban_equipement"],
 					"id_fk_recette_laban_equipement" => $e["id_fk_recette_laban_equipement"],
@@ -58,25 +58,25 @@ class Bral_Lieux_Joaillier extends Bral_Lieux_Lieu {
 					"nom_systeme_type_piece" => $e["nom_systeme_type_piece"],
 					"selected" => $selected
 				);
-				
+
 				if ($id_equipement_courant == $e["id_laban_equipement"]) {
 					$equipementCourant = $t;
 				}
 				$tabEquipementsLaban[] = $t;
 			}
 		}
-		
+
 		$this->view->nbEquipementsLaban = count($tabEquipementsLaban);
 		$this->view->equipementsLaban = $tabEquipementsLaban;
 
 		$this->view->equipementEnCours = null;
-		
+
 		if (isset($equipementCourant)) {
 			Zend_Loader::loadClass("EquipementRune");
 			$tabEquipementsRune = null;
 			$equipementRuneTable = new EquipementRune();
 			$equipementRunes = $equipementRuneTable->findByIdEquipement($id_equipement_courant);
-			
+				
 			foreach($equipementRunes as $e) {
 				$tabEquipementsRune[] = array(
 					"id_rune_equipement_rune" => $e["id_rune_equipement_rune"],
@@ -85,16 +85,16 @@ class Bral_Lieux_Joaillier extends Bral_Lieux_Lieu {
 					"effet_type_rune" => $e["effet_type_rune"],
 				);
 			}
-			
+				
 			$this->view->nbEquipementRune = count($tabEquipementsRune);
 			$this->view->equipementRunes = $tabEquipementsRune;
 			$this->view->equipementCourant = $equipementCourant;
-			
+				
 			Zend_Loader::loadClass("LabanRune");
 			$tabLabanRune = null;
 			$labanRuneTable = new LabanRune();
-			$labanRunes = $labanRuneTable->findByIdHobbit($this->view->user->id_hobbit);
-			
+			$labanRunes = $labanRuneTable->findByIdHobbit($this->view->user->id_hobbit, "oui");
+				
 			foreach($labanRunes as $l) {
 				if ($l["est_identifiee_rune"] == "oui") {
 					$tabLabanRune[$l["id_rune_laban_rune"]] = array(
@@ -116,35 +116,35 @@ class Bral_Lieux_Joaillier extends Bral_Lieux_Lieu {
 	}
 
 	function prepareResultat() {
-	
+
 		$idEquipementLaban = $this->request->get("valeur_1");
 		$nbRunes = $this->request->get("valeur_2");
 		$runes = $this->request->get("valeur_3");
-		
+
 		if ((int) $idEquipementLaban."" != $this->request->get("valeur_1")."") {
 			throw new Zend_Exception(get_class($this)." Equipement Laban invalide=".$idEquipementLaban);
 		} else {
 			$idEquipementLaban = (int)$idEquipementLaban;
 		}
-		
+
 		if ($idEquipementLaban != $this->view->equipementCourant["id_laban_equipement"]) {
 			throw new Zend_Exception(get_class($this)." idEquipement interdit A=".$idEquipementLaban. " B=".$this->view->equipementCourant["id_laban_equipement"]);
 		}
-		
+
 		if ((int) $nbRunes."" != $this->request->get("valeur_2")."") {
 			throw new Zend_Exception(get_class($this)." Nb Rune invalide=".$nbRunes);
 		} else {
 			$nbRunes = (int)$nbRunes;
 		}
-		
+
 		if ($runes == "" || $runes == null) {
 			throw new Zend_Exception(get_class($this)." Runes invalides=".$runes);
 		}
-		
+
 		$tabRunesJs = explode(",", $runes);
 		$tabRunes = null;
 		// on regarde si les runes sont présentes dans le laban
-		
+
 		$tmp = $this->view->labanRunes;
 		$nb = 0;
 		foreach($tabRunesJs as $u) {
@@ -161,16 +161,16 @@ class Bral_Lieux_Joaillier extends Bral_Lieux_Lieu {
 				throw new Zend_Exception(get_class($this)." Rune invalide =".$u);
 			}
 		}
-		
+
 		if ($nb != $nbRunes) {
-				throw new Zend_Exception(get_class($this)." Nombre de runes invalides A n1=".$nb. " n2=".$nbRunes);
+			throw new Zend_Exception(get_class($this)." Nombre de runes invalides A n1=".$nb. " n2=".$nbRunes);
 		}
-		
+
 		// tous les emplacements runiques doivent etre utilises
 		if ($nb == 0 || $nb != $this->view->equipementCourant["nb_runes"]) {
-				throw new Zend_Exception(get_class($this)." Nombre de runes invalides B n1=".$nb. " n2=".$this->view->equipementCourant["nb_runes"]);
+			throw new Zend_Exception(get_class($this)." Nombre de runes invalides B n1=".$nb. " n2=".$this->view->equipementCourant["nb_runes"]);
 		}
-		
+
 		$this->view->suffixe = "";
 		$this->calculSertir($tabRunes);
 		$this->view->nbRunes = $nb;
@@ -182,19 +182,19 @@ class Bral_Lieux_Joaillier extends Bral_Lieux_Lieu {
 	private function calculSertir($tabRunes) {
 		$equipementRuneTable = new EquipementRune();
 		$labanRuneTable = new LabanRune();
-		
+
 		// on regarde si les runes ne signifient pas un mot runique
 		$motRuniqueTable = new MotRunique();
-		
+
 		$id_fk_mot_runique_laban_equipement = null;
 		$nom_mot_runique = null;
-		
+
 		if ($this->view->equipementCourant["nom_systeme_type_piece"] == "arme_tir") { // si c'est une "arme de tir", on prend les mots runiques de "arme"
 			$nomSystemeTypePiece = "arme";
 		} else {
-			$nomSystemeTypePiece = $this->view->equipementCourant["nom_systeme_type_piece"];	
+			$nomSystemeTypePiece = $this->view->equipementCourant["nom_systeme_type_piece"];
 		}
-		
+
 		$motsRowset = $motRuniqueTable->findByIdTypePieceAndRunes($nomSystemeTypePiece, $tabRunes);
 		if (count($motsRowset) > 0) {
 			foreach ($motsRowset as $m) {
@@ -204,7 +204,7 @@ class Bral_Lieux_Joaillier extends Bral_Lieux_Lieu {
 				break; // s'il y a plusieurs mots (ce qui devrait jamais arriver), on prend le premier
 			}
 		}
-		
+
 		$ordre = 0;
 		foreach($tabRunes as $k => $v) {
 			$ordre++;
@@ -215,12 +215,12 @@ class Bral_Lieux_Joaillier extends Bral_Lieux_Lieu {
 				'ordre_equipement_rune' => $ordre
 			);
 			$equipementRuneTable->insert($data);
-			
+				
 			// Suppression des runes du laban
 			$where = "id_rune_laban_rune = ".$v["id_rune_laban_rune"];
 			$labanRunes = $labanRuneTable->delete($where);
 		}
-		
+
 		if ($id_fk_mot_runique_laban_equipement != null) {
 			$labanEquipementTable = new LabanEquipement();
 			$data = array(
@@ -228,7 +228,7 @@ class Bral_Lieux_Joaillier extends Bral_Lieux_Lieu {
 			);
 			$where = "id_laban_equipement=".$this->view->equipementCourant["id_laban_equipement"];
 			$labanEquipementTable->update($data, $where);
-			
+				
 			Zend_Loader::loadClass("StatsMotsRuniques");
 			$statsMotsRuniques = new StatsMotsRuniques();
 			$moisEnCours  = mktime(0, 0, 0, date("m"), 2, date("Y"));
@@ -239,36 +239,36 @@ class Bral_Lieux_Joaillier extends Bral_Lieux_Lieu {
 			$dataMotsRuniques["id_fk_type_piece_stats_mots_runiques"] = $this->view->equipementCourant["id_fk_type_piece"];
 			$statsMotsRuniques->insertOrUpdate($dataMotsRuniques);
 		}
-		
+
 		if ($nom_mot_runique != null && $nom_mot_runique == "mot_f") {
 			Zend_Loader::loadClass("EffetMotF");
 			Zend_Loader::loadClass("TypeMonstre");
-			
+				
 			$this->view->effetMotF = false;
 			$typeMonstreTable = new TypeMonstre();
-			
+				
 			$typeMonstreRowset = $typeMonstreTable->fetchall();
 			$typeMonstreRowset = $typeMonstreRowset->toArray();
-			
+				
 			$typesMonstre = null;
-			
+				
 			foreach($typeMonstreRowset as $t) {
 				$typesMonstre[] = array(
 					"id_type_monstre" => $t["id_type_monstre"],
-					//"nom_type_monstre" => $t["nom_type_monstre"],
+				//"nom_type_monstre" => $t["nom_type_monstre"],
 				);
 			}
-			
+				
 			$nTypeMonstre = Bral_Util_De::get_de_specifique(0, count($typesMonstre)-1);
 			$idTypeMonstre = $typesMonstre[$nTypeMonstre]["id_type_monstre"];
-			
+				
 			$effetMotFTable = new EffetMotF();
-			$data = array("id_fk_hobbit_effet_mot_f" => $this->view->user->id_hobbit, 
+			$data = array("id_fk_hobbit_effet_mot_f" => $this->view->user->id_hobbit,
 						  "id_fk_type_monstre_effet_mot_f" => $idTypeMonstre);
 			$effetMotDTable->insert($data);
 		}
 	}
-	
+
 	function getListBoxRefresh() {
 		return array("box_profil", "box_metier", "box_laban");
 	}
