0efa5c33c261bebcf9822cb43baf089ae889b6ac
BouleHondevice <boule.hondevice@braldahim.com>
2009-05-07 21:01:36 +0000

    Avancement #420
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@1547 8885cbc4-9223-0410-882a-37b6fa6ce781

--- braldahim/library/Bral/Box/Banque.php
+++ braldahim/library/Bral/Box/Banque.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This file is part of Braldahim, under Gnu Public Licence v3. 
+ * This file is part of Braldahim, under Gnu Public Licence v3.
  * See licence.txt or http://www.gnu.org/licenses/gpl-3.0.html
  *
  * $Id$
@@ -11,11 +11,11 @@
  * $LastChangedBy$
  */
 class Bral_Box_Banque extends Bral_Box_Box {
-	
+
 	public function getTitreOnglet() {
 		return "Banque";
 	}
-	
+
 	function getNomInterne() {
 		return "box_lieu";
 	}
@@ -23,7 +23,7 @@ class Bral_Box_Banque extends Bral_Box_Box {
 	function getChargementInBoxes() {
 		return false;
 	}
-	
+
 	function setDisplay($display) {
 		$this->view->display = $display;
 	}
@@ -41,14 +41,14 @@ class Bral_Box_Banque extends Bral_Box_Box {
 		$this->view->nom_interne = $this->getNomInterne();
 		return $this->view->render("interface/banque.phtml");
 	}
-	
+
 	protected function preData() {
 		Zend_Loader::loadClass("Lieu");
-		
+
 		$lieuxTable = new Lieu();
 		$lieuRowset = $lieuxTable->findByCase($this->view->user->x_hobbit, $this->view->user->y_hobbit);
 		unset($lieuxTable);
-		
+
 		if (count($lieuRowset) <= 0) {
 			throw new Zend_Exception("Bral_Box_Banque::nombre de lieux invalide <= 0 !");
 		} elseif (count($lieuRowset) > 1) {
@@ -59,41 +59,42 @@ class Bral_Box_Banque extends Bral_Box_Box {
 			$this->view->nomLieu = $lieu["nom_lieu"];
 			$this->view->paUtilisationBanque = $lieu["pa_utilisation_type_lieu"];
 		}
-		
+
 	}
-	
+
 	protected function data() {
-		
+
 		Zend_Loader::loadClass("Coffre");
 		Zend_Loader::loadClass("CoffreEquipement");
 		Zend_Loader::loadClass("CoffreMinerai");
 		Zend_Loader::loadClass("CoffrePartieplante");
 		Zend_Loader::loadClass("CoffreAliment");
+		Zend_Loader::loadClass("CoffreMunition");
 		Zend_Loader::loadClass("CoffrePotion");
 		Zend_Loader::loadClass("CoffreRune");
 		Zend_Loader::loadClass("HobbitsMetiers");
 		Zend_Loader::loadClass("Metier");
 		Zend_Loader::loadClass("TypePlante");
 		Zend_Loader::loadClass("TypePartieplante");
-		
+
 		$hobbitsMetiersTable = new HobbitsMetiers();
 		$hobbitsMetierRowset = $hobbitsMetiersTable->findMetiersByHobbitId($this->view->user->id_hobbit);
 		unset($hobbitsMetiersTable);
-		
+
 		$metiersTable = new Metier();
 		$metiersRowset = $metiersTable->fetchall(null, "nom_masculin_metier");
 		unset($metiersTable);
 		$metiersRowset = $metiersRowset->toArray();
 		$tabHobbitMetiers = null;
 		$tabMetiers = null;
-		
+
 		foreach($metiersRowset as $m) {
 			if ($this->view->user->sexe_hobbit == 'feminin') {
 				$nom_metier = $m["nom_feminin_metier"];
 			} else {
 				$nom_metier = $m["nom_masculin_metier"];
 			}
-			
+				
 			$possedeMetier = false;
 			foreach($hobbitsMetierRowset as $h) {
 				if ($h["id_metier"] == $m["id_metier"]) {
@@ -101,14 +102,14 @@ class Bral_Box_Banque extends Bral_Box_Box {
 					break;
 				}
 			}
-			
+				
 			if ($possedeMetier == true) {
 				$tabHobbitMetiers[$m["nom_systeme_metier"]] = array(
 						"id_metier" => $m["id_metier"],
 						"nom" => $nom_metier,
 						"nom_systeme" => $m["nom_systeme_metier"],
 						"a_afficher" => true,
-					);
+				);
 			} else {
 				$tabMetiers[$m["nom_systeme_metier"]] = array(
 					"id_metier" => $m["id_metier"],
@@ -119,13 +120,13 @@ class Bral_Box_Banque extends Bral_Box_Box {
 			}
 		}
 		unset($metiersRowset);
-		
+
 		$tabMineraisBruts = null;
 		$tabLingots = null;
 		$coffreMineraiTable = new CoffreMinerai();
 		$minerais = $coffreMineraiTable->findByIdHobbit($this->view->user->id_hobbit);
 		unset($coffreMineraiTable);
-	
+
 		foreach ($minerais as $m) {
 			if ($m["quantite_brut_coffre_minerai"] > 0) {
 				$tabMineraisBruts[] = array(
@@ -134,9 +135,9 @@ class Bral_Box_Banque extends Bral_Box_Box {
 					"quantite" => $m["quantite_brut_coffre_minerai"],
 					"poids" => $m["quantite_brut_coffre_minerai"] * Bral_Util_Poids::POIDS_MINERAI,
 				);
-			
+					
 				if (isset($tabMetiers["mineur"])) {
-					$tabMetiers["mineur"]["a_afficher"] = true; 
+					$tabMetiers["mineur"]["a_afficher"] = true;
 				}
 			}
 			if ($m["quantite_lingots_coffre_minerai"] > 0) {
@@ -146,9 +147,9 @@ class Bral_Box_Banque extends Bral_Box_Box {
 					"quantite" => $m["quantite_lingots_coffre_minerai"],
 					"poids" => $m["quantite_lingots_coffre_minerai"] * Bral_Util_Poids::POIDS_LINGOT,
 				);
-			
+					
 				if (isset($tabMetiers["forgeron"])) {
-					$tabMetiers["forgeron"]["a_afficher"] = true; 
+					$tabMetiers["forgeron"]["a_afficher"] = true;
 				}
 			}
 		}
@@ -158,7 +159,7 @@ class Bral_Box_Banque extends Bral_Box_Box {
 		$coffreTable = new Coffre();
 		$coffre = $coffreTable->findByIdHobbit($this->view->user->id_hobbit);
 		unset($coffreTable);
-		
+
 		foreach ($coffre as $p) {
 			$tabCoffre = array(
 				"nb_peau" => $p["quantite_peau_coffre"],
@@ -169,33 +170,33 @@ class Bral_Box_Banque extends Bral_Box_Box {
 				"nb_planche" => $p["quantite_planche_coffre"],
 				"nb_castar" => $p["quantite_castar_coffre"],
 			);
-			
+				
 			if ($p["quantite_peau_coffre"] > 0 || $p["quantite_viande_coffre"] > 0) {
 				if (isset($tabMetiers["chasseur"])) {
-					$tabMetiers["chasseur"]["a_afficher"] = true; 
+					$tabMetiers["chasseur"]["a_afficher"] = true;
 				}
 			}
-			
+				
 			if ($p["quantite_viande_preparee_coffre"] > 0) {
 				if (isset($tabMetiers["cuisinier"])) {
-					$tabMetiers["cuisinier"]["a_afficher"] = true; 
+					$tabMetiers["cuisinier"]["a_afficher"] = true;
 				}
 			}
-			
+				
 			if ($p["quantite_cuir_coffre"] > 0 || $p["quantite_fourrure_coffre"] > 0) {
 				if (isset($tabMetiers["tanneur"])) {
-					$tabMetiers["tanneur"]["a_afficher"] = true; 
+					$tabMetiers["tanneur"]["a_afficher"] = true;
 				}
 			}
 
 			if ($p["quantite_planche_coffre"] > 0) {
 				if (isset($tabMetiers["menuisier"])) {
-					$tabMetiers["menuisier"]["a_afficher"] = true; 
+					$tabMetiers["menuisier"]["a_afficher"] = true;
 				}
 			}
 		}
 		unset($coffre);
-		
+
 		$tabRunesIdentifiees = null;
 		$tabRunesNonIdentifiees = null;
 		$coffreRuneTable = new CoffreRune();
@@ -224,28 +225,29 @@ class Bral_Box_Banque extends Bral_Box_Box {
 		unset($runes);
 
 		$this->view->tabHobbitMetiers = $tabHobbitMetiers;
-		
-		
+
+
 		$this->view->mineraisBruts = $tabMineraisBruts;
 		$this->view->lingots = $tabLingots;
-		
+
 		$this->view->nb_runes = count($tabRunesIdentifiees) + count($tabRunesNonIdentifiees);
 		$this->view->runesIdentifiees = $tabRunesIdentifiees;
 		$this->view->runesNonIdentifiees = $tabRunesNonIdentifiees;
 		$this->view->coffre = $tabCoffre;
 		$this->view->laban = $tabCoffre; // pour les poches
-		
+
 		$this->renderPlante($tabMetiers);
 		$this->view->tabMetiers = $tabMetiers;
 		$this->renderEquipement();
+		$this->renderMunition();
 		$this->renderPotion();
 		$this->renderAliment();
-		
+
 		$this->view->estEquipementsPotionsEtal = false;
 		$this->view->estEquipementsPotionsEtalAchat = false;
-		
+
 		$this->view->nom_interne = $this->getNomInterne();
-		
+
 		unset($tabHobbitMetiers);
 		unset($tabMetiers);
 		unset($tabMineraisBruts);
@@ -253,22 +255,22 @@ class Bral_Box_Banque extends Bral_Box_Box {
 		unset($tabRunesIdentifiees);
 		unset($tabRunesNonIdentifiees);
 	}
-	
+
 	private function renderPlante(&$tabMetiers) {
 		$typePlantesTable = new TypePlante();
 		$typePlantesRowset = $typePlantesTable->findAll();
 		unset($typePlantesTable);
-		
+
 		$typePartiePlantesTable = new TypePartieplante();
 		$typePartiePlantesRowset = $typePartiePlantesTable->fetchall();
 		unset($typePartiePlantesTable);
 		$typePartiePlantesRowset = $typePartiePlantesRowset->toArray();
-	
+
 		$tabTypePlantes = null;
 		$coffrePartiePlanteTable = new CoffrePartieplante();
 		$partiePlantes = $coffrePartiePlanteTable->findByIdHobbit($this->view->user->id_hobbit);
 		unset($coffrePartiePlanteTable);
-		
+
 		foreach($typePartiePlantesRowset as $p) {
 			foreach($typePlantesRowset as $t) {
 				$val = false;
@@ -284,7 +286,7 @@ class Bral_Box_Banque extends Bral_Box_Box {
 				if ($t["id_fk_partieplante4_type_plante"] == $p["id_type_partieplante"]) {
 					$val = true;
 				}
-				
+
 				if (!isset($tabTypePlantes[$t["categorie_type_plante"]][$t["nom_type_plante"]])) {
 					$tab = array(
 						'nom_type_plante' => $t["nom_type_plante"],
@@ -292,7 +294,7 @@ class Bral_Box_Banque extends Bral_Box_Box {
 					);
 					$tabTypePlantes[$t["categorie_type_plante"]][$t["nom_type_plante"]] = $tab;
 				}
-				
+
 				$tabTypePlantes[$t["categorie_type_plante"]]["a_afficher"] = false;
 				$tabTypePlantes[$t["categorie_type_plante"]]["type_plante"][$t["nom_type_plante"]]["a_afficher"] = false;
 				$tabTypePlantes[$t["categorie_type_plante"]]["type_plante"][$t["nom_type_plante"]]["parties"][$p["nom_systeme_type_partieplante"]]["possible"] = $val;
@@ -301,10 +303,10 @@ class Bral_Box_Banque extends Bral_Box_Box {
 		}
 		unset($typePartiePlantesRowset);
 		unset($typePlantesRowset);
-		
+
 		$tabTypePlantesBruts = $tabTypePlantes;
 		$tabTypePlantesPrepares = $tabTypePlantes;
-		
+
 		foreach ($partiePlantes as $p) {
 			if ($p["quantite_coffre_partieplante"] > 0) {
 				$tabTypePlantesBruts[$p["categorie_type_plante"]]["a_afficher"] = true;
@@ -315,14 +317,14 @@ class Bral_Box_Banque extends Bral_Box_Box {
 					$tabMetiers["herboriste"]["a_afficher"] = true;
 				}
 			}
-			
+				
 			if ($p["quantite_preparee_coffre_partieplante"] > 0) {
 				$tabTypePlantesPrepares[$p["categorie_type_plante"]]["a_afficher"] = true;
 				$tabTypePlantesPrepares[$p["categorie_type_plante"]]["type_plante"][$p["nom_type_plante"]]["a_afficher"] = true;
 				$tabTypePlantesPrepares[$p["categorie_type_plante"]]["type_plante"][$p["nom_type_plante"]]["parties"][$p["nom_systeme_type_partieplante"]]["quantite"] = $p["quantite_preparee_coffre_partieplante"];
 				$tabTypePlantesPrepares[$p["categorie_type_plante"]]["type_plante"][$p["nom_type_plante"]]["parties"][$p["nom_systeme_type_partieplante"]]["poids"] = $p["quantite_preparee_coffre_partieplante"] * Bral_Util_Poids::POIDS_PARTIE_PLANTE_PREPAREE;
 				if (isset($tabMetiers["apothicaire"])) {
-					$tabMetiers["apothicaire"]["a_afficher"] = true; 
+					$tabMetiers["apothicaire"]["a_afficher"] = true;
 				}
 			}
 		}
@@ -331,15 +333,15 @@ class Bral_Box_Banque extends Bral_Box_Box {
 		$this->view->typePlantesBruts = $tabTypePlantesBruts;
 		$this->view->typePlantesPrepares = $tabTypePlantesPrepares;
 	}
-	
+
 	private function renderEquipement() {
 		$tabEquipements = null;
 		$coffreEquipementTable = new CoffreEquipement();
 		$equipements = $coffreEquipementTable->findByIdHobbit($this->view->user->id_hobbit);
 		unset($coffreEquipementTable);
-		
+
 		Zend_Loader::loadClass("Bral_Util_Equipement");
-		
+
 		$tabWhere = null;
 		foreach ($equipements as $e) {
 			$tabEquipements[$e["id_coffre_equipement"]] = array(
@@ -365,23 +367,42 @@ class Bral_Box_Banque extends Bral_Box_Box {
 			$tabWhere[] = $e["id_coffre_equipement"];
 		}
 		unset($equipements);
-		
+
 		if ($tabWhere != null) {
 			Zend_Loader::loadClass("Bral_Util_Equipement");
 			Bral_Util_Equipement::populateRune($tabEquipements, $tabWhere);
 			Bral_Util_Equipement::populateBonus($tabEquipements, $tabWhere);
 		}
-		
+
 		$this->view->nb_equipements = count($tabEquipements);
 		$this->view->equipements = $tabEquipements;
 	}
-	
+
+	private function renderMunition() {
+		$tabMunitions = null;
+		$coffreMunitionTable = new CoffreMunition();
+		$munitions = $coffreMunitionTable->findByIdHobbit($this->view->user->id_hobbit);
+		unset($coffreMunitionTable);
+
+		foreach ($munitions as $m) {
+			$tabMunitions[] = array(
+				"type" => $m["nom_type_munition"],
+				"quantite" => $m["quantite_coffre_munition"],
+				"poids" =>  $m["quantite_coffre_munition"] * Bral_Util_Poids::POIDS_MUNITION,
+			);
+		}
+		unset($munitions);
+
+		$this->view->nb_munitions = count($tabMunitions);
+		$this->view->munitions = $tabMunitions;
+	}
+
 	private function renderPotion() {
 		$tabPotions = null;
 		$coffrePotionTable = new CoffrePotion();
 		$potions = $coffrePotionTable->findByIdHobbit($this->view->user->id_hobbit);
 		unset($coffrePotionTable);
-		
+
 		foreach ($potions as $p) {
 			$tabPotions[$p["id_coffre_potion"]] = array(
 					"id_potion" => $p["id_coffre_potion"],
@@ -393,17 +414,17 @@ class Bral_Box_Banque extends Bral_Box_Box {
 			);
 		}
 		unset($potions);
-		
+
 		$this->view->nb_potions = count($tabPotions);
 		$this->view->potions = $tabPotions;
 	}
-	
+
 	private function renderAliment() {
 		$tabAliments = null;
 		$coffreAlimentTable = new CoffreAliment();
 		$aliments = $coffreAlimentTable->findByIdHobbit($this->view->user->id_hobbit);
 		unset($coffreAlimentTable);
-		
+
 		foreach ($aliments as $p) {
 			$tabAliments[$p["id_coffre_aliment"]] = array(
 					"id_aliment" => $p["id_coffre_aliment"],
@@ -414,7 +435,7 @@ class Bral_Box_Banque extends Bral_Box_Box {
 			);
 		}
 		unset($aliments);
-		
+
 		$this->view->nb_aliments = count($tabAliments);
 		$this->view->aliments = $tabAliments;
 	}
