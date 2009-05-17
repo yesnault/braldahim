639332f4e2affca5f37371167f0db4c609c7d1ca
BouleHondevice <boule.hondevice@braldahim.com>
2009-05-17 10:40:25 +0000

    Ajout rondins au sol
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@1583 8885cbc4-9223-0410-882a-37b6fa6ce781

--- braldahim/application/models/Element.php
+++ braldahim/application/models/Element.php
@@ -41,7 +41,8 @@ class Element extends Zend_Db_Table {
 		quantite_cuir_element as quantiteCuir,
 		quantite_castar_element as quantiteCastar,
 		quantite_fourrure_element as quantiteFourrure,
-		quantite_planche_element as quantitePlanche')
+		quantite_planche_element as quantitePlanche,
+		quantite_rondin_element as quantiteRondin')
 		->where('x_element = ?',$data["x_element"])
 		->where('y_element = ?',$data["y_element"])
 		->group(array('quantitePeau', 'quantiteViande', 'quantiteViandePreparee'));
@@ -59,7 +60,8 @@ class Element extends Zend_Db_Table {
 			$quantiteCastar = $resultat[0]["quantiteCastar"];
 			$quantiteFourrure = $resultat[0]["quantiteFourrure"];
 			$quantitePlanche = $resultat[0]["quantitePlanche"];
-				
+			$quantiteRondin = $resultat[0]["quantiteRondin"];
+
 			$dataUpdate['quantite_viande_element'] = $quantiteViande;
 			$dataUpdate['quantite_peau_element'] = $quantitePeau;
 			$dataUpdate['quantite_viande_preparee_element'] = $quantiteViandePreparee;
@@ -67,7 +69,8 @@ class Element extends Zend_Db_Table {
 			$dataUpdate['quantite_fourrure_element'] = $quantiteFourrure;
 			$dataUpdate['quantite_castar_element'] = $quantiteCastar;
 			$dataUpdate['quantite_planche_element'] = $quantitePlanche;
-				
+			$dataUpdate['quantite_rondin_element'] = $quantiteRondin;
+
 			if (isset($data["quantite_viande_element"])) {
 				$dataUpdate['quantite_viande_element'] = $quantiteViande + $data["quantite_viande_element"];
 			}
@@ -89,7 +92,10 @@ class Element extends Zend_Db_Table {
 			if (isset($data['quantite_planche_element'])) {
 				$dataUpdate['quantite_planche_element'] = $quantitePlanche + $data["quantite_planche_element"];
 			}
-				
+			if (isset($data['quantite_rondin_element'])) {
+				$dataUpdate['quantite_rondin_element'] = $quantiteRondin + $data["quantite_rondin_element"];
+			}
+
 			$where = ' x_element = '.$data["x_element"];
 			$where .= ' AND y_element = '.$data["y_element"];
 
@@ -98,8 +104,9 @@ class Element extends Zend_Db_Table {
 			$dataUpdate['quantite_viande_preparee_element'] <= 0 &&
 			$dataUpdate['quantite_cuir_element'] <= 0 &&
 			$dataUpdate['quantite_fourrure_element'] <= 0 &&
-			$dataUpdate['quantite_planche_element'] <= 0 && 
-			$dataUpdate['quantite_castar_element'] <= 0) { // delete
+			$dataUpdate['quantite_planche_element'] <= 0 &&
+			$dataUpdate['quantite_castar_element'] <= 0 && 
+			$dataUpdate['quantite_rondin_element'] <= 0) { // delete
 				$this->delete($where);
 			} else { // update
 				$this->update($dataUpdate, $where);
