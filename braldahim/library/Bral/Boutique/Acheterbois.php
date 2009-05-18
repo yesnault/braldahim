04be4b9ca923f297551e2e60fccf9121945aca38
BouleHondevice <boule.hondevice@braldahim.com>
2009-05-18 12:31:38 +0000

    Acheter Bois
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@1593 8885cbc4-9223-0410-882a-37b6fa6ce781

--- braldahim/library/Bral/Boutique/Acheterbois.php
+++ braldahim/library/Bral/Boutique/Acheterbois.php
@@ -25,6 +25,30 @@ class Bral_Boutique_Acheterbois extends Bral_Boutique_Boutique {
 		Zend_Loader::loadClass("Bral_Util_BoutiqueBois");
 		Zend_Loader::loadClass("StockBois");
 		Zend_Loader::loadClass("BoutiqueBois");
+		Zend_Loader::loadClass("Laban");
+
+		$idDestinationCourante = $this->request->get("id_destination_courante");
+
+		$selectedLaban = "";
+		$selectedCharrette = "";
+		if ($idDestinationCourante == "laban") {
+			$selectedLaban = "selected";
+		} else if ($idDestinationCourante == "charrette") {
+			$selectedCharrette = "selected";
+		}
+		$tabDestinationTransfert[] = array("id_destination" => "laban", "texte" => "votre laban", "selected" => $selectedLaban);
+
+		$charretteTable = new Charrette();
+		$charrettes = $charretteTable->findByIdHobbit($this->view->user->id_hobbit);
+
+		$charrette = null;
+		if (count($charrettes) == 1) {
+			$charrette = $charrettes[0];
+			$tabDestinationTransfert[] = array("id_destination" => "charrette", "texte" => "votre charrette", "selected" => $selectedCharrette);
+		}
+		$this->view->destinationTransfertCourante = $idDestinationCourante;
+		$this->view->destinationTransfert = $tabDestinationTransfert;
+		$this->view->charrette = $charrette;
 
 		$this->preparePrix();
 	}
@@ -62,7 +86,33 @@ class Bral_Boutique_Acheterbois extends Bral_Boutique_Boutique {
 			throw new Zend_Exception("Bral_Boutique_Acheterbois :: Nombre invalide 0");
 		}
 
-		$this->transfert();
+		$idDestination = $this->request->get("valeur_2");
+
+		if ($this->request->get("id_destination_courante") != $idDestination) {
+			throw new Zend_Exception(get_class($this)." destination invalide 1");
+		}
+
+		if ($this->view->charrette == null && $this->request->get("id_destination_courante") == "charrette") {
+			throw new Zend_Exception(get_class($this)." destination invalide 2");
+		}
+
+		// on regarde si l'on connait la destination
+		$flag = false;
+		$destination = null;
+		foreach($this->view->destinationTransfert as $d) {
+			if ($d["id_destination"] == $idDestination) {
+				$destination = $d;
+				$flag = true;
+				break;
+			}
+		}
+
+		if ($flag == false) {
+			throw new Zend_Exception(get_class($this)." destination inconnue=".$destination);
+		}
+
+		$this->transfert($idDestination);
+		$this->view->destination = $destination;
 	}
 
 	private function preparePrix() {
@@ -96,16 +146,29 @@ class Bral_Boutique_Acheterbois extends Bral_Boutique_Boutique {
 			$this->view->acheterPossible = false;
 		} else {
 			$this->view->acheterPossible = true;
-				
-			$this->view->tabPoidsCharrette = Bral_Util_Poids::calculPoidsCharrette($this->view->user->id_hobbit);
-			if ($this->view->tabPoidsCharrette != null) {
+
+			if ($this->view->destinationTransfertCourante != null) {
 				$this->view->acheterPossible = true;
 				$this->view->possedeCharrette = true;
 
-				$nbDansCharretteMaximum = floor($this->view->tabPoidsCharrette["place_restante"] / Bral_Util_Poids::POIDS_RONDIN);
+				if ($this->view->destinationTransfertCourante == "laban") {
+					$labanTable = new Laban();
+					$labanRowset = $labanTable->findByIdHobbit($this->view->user->id_hobbit);
+					if (count($labanRowset) == 1) {
+						$laban = $labanRowset[0];
+					} else {
+						throw new Zend_Exception("Bral_Boutique_Acheterbois :: Nombre laban invalide idh:".$this->view->user->id_hobbit);
+					}
+					$this->view->nbMaximumContenant = floor(($this->view->user->poids_transportable_hobbit - $this->view->user->poids_transporte_hobbit) / Bral_Util_Poids::POIDS_RONDIN);
+					$this->view->nbRondinsActuels = $laban["quantite_rondin_laban"];
+				} else if ($this->view->destinationTransfertCourante == "charrette") {
+					$tabPoidsCharrette = Bral_Util_Poids::calculPoidsCharrette($this->view->user->id_hobbit);
+					$this->view->nbMaximumContenant = floor($tabPoidsCharrette["place_restante"] / Bral_Util_Poids::POIDS_RONDIN);
+					$this->view->nbRondinsActuels = $this->view->charrette["quantite_rondin_charrette"];
+				}
 
-				if ($this->view->nombreMaximum > $nbDansCharretteMaximum) {
-					$this->view->nombreMaximum = $nbDansCharretteMaximum;
+				if ($this->view->nombreMaximum > $this->view->nbMaximumContenant) {
+					$this->view->nombreMaximum = $this->view->nbMaximumContenant;
 					if ($this->view->nombreMaximum > $this->view->nbStockRestant) {
 						$this->view->nombreMaximum = $this->view->nbStockRestant;
 					}
@@ -121,17 +184,27 @@ class Bral_Boutique_Acheterbois extends Bral_Boutique_Boutique {
 		}
 	}
 
-	private function transfert() {
+	private function transfert($idDestination) {
 		$this->view->coutCastars = floor($this->view->quantiteAchetee * $this->view->prixUnitaire);
 		$this->view->user->castars_hobbit = $this->view->user->castars_hobbit - $this->view->coutCastars;
 
-		$charretteTable = new Charrette();
-		$data = array(
-			'quantite_rondin_charrette' => $this->view->quantiteAchetee,
-			'id_fk_hobbit_charrette' => $this->view->user->id_hobbit,
-		);
-		$charretteTable->updateCharrette($data);
-		unset($charretteTable);
+		if ($idDestination == "charrette") {
+			$charretteTable = new Charrette();
+			$data = array(
+				'quantite_rondin_charrette' => $this->view->quantiteAchetee,
+				'id_fk_hobbit_charrette' => $this->view->user->id_hobbit,
+			);
+			$charretteTable->updateCharrette($data);
+			unset($charretteTable);
+		} else { // laban
+			$labanTable = new Laban();
+			$data = array(
+				'quantite_rondin_laban' => $this->view->quantiteAchetee,
+				'id_fk_hobbit_laban' => $this->view->user->id_hobbit,
+			);
+			$labanTable->insertOrUpdate($data);
+			unset($labanTable);
+		}
 
 		$data = array(
 			"date_achat_boutique_bois" => date("Y-m-d H:i:s"),
@@ -151,10 +224,19 @@ class Bral_Boutique_Acheterbois extends Bral_Boutique_Boutique {
 		);
 		$stockBoisTable = new StockBois();
 		$stockBoisTable->updateStock($data);
+		
+		if ($idDestination == "charrette") {
+			Bral_Util_Poids::calculPoidsCharrette($this->view->user->id_hobbit, true);
+		}
 
 	}
 
 	function getListBoxRefresh() {
-		return array("box_profil", "box_laban", "box_charrette", "box_evenements", "box_bbois");
+		if ($this->view->destination["id_destination"] == "charrette") {
+			$boxToRefresh = "box_charrette";
+		} else {
+			$boxToRefresh = "box_laban";
+		}
+		return array("box_profil", $boxToRefresh, "box_charrette", "box_evenements", "box_bbois");
 	}
 }
