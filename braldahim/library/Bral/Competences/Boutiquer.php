f3b628f963307c0fe5337c128730f2e9b777b12e
BouleHondevice <boule.hondevice@braldahim.com>
2009-05-20 19:33:16 +0000

    Correction gains %
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@1611 8885cbc4-9223-0410-882a-37b6fa6ce781

--- braldahim/library/Bral/Competences/Boutiquer.php
+++ braldahim/library/Bral/Competences/Boutiquer.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This file is part of Braldahim, under Gnu Public Licence v3. 
+ * This file is part of Braldahim, under Gnu Public Licence v3.
  * See licence.txt or http://www.gnu.org/licenses/gpl-3.0.html
  *
  * $Id$
@@ -15,30 +15,30 @@ abstract class Bral_Competences_Boutiquer extends Bral_Competences_Competence {
 	function prepareCommun() {
 		Zend_Loader::loadClass("Echoppe");
 		Zend_Loader::loadClass("PetitEquipement");
-		
+
 		// On regarde si le hobbit est dans une de ses echopppes
 		$echoppeTable = new Echoppe();
 		$echoppes = $echoppeTable->findByCase($this->view->user->x_hobbit, $this->view->user->y_hobbit);
-		
+
 		$this->view->boutiquerEchoppeOk = false;
 		if ($echoppes == null || count($echoppes) == 0) {
 			$this->view->boutiquerEchoppeOk = false;
 			return;
 		}
-		
+
 		$idEchoppe = -1;
 		$metier = substr($this->nom_systeme, 9, strlen($this->nom_systeme) - 9);
 		foreach($echoppes as $e) {
-			if ($e["id_fk_hobbit_echoppe"] == $this->view->user->id_hobbit && 
-				$e["nom_systeme_metier"] == $metier && 
-				$e["x_echoppe"] == $this->view->user->x_hobbit && 
-				$e["y_echoppe"] == $this->view->user->y_hobbit) {
+			if ($e["id_fk_hobbit_echoppe"] == $this->view->user->id_hobbit &&
+			$e["nom_systeme_metier"] == $metier &&
+			$e["x_echoppe"] == $this->view->user->x_hobbit &&
+			$e["y_echoppe"] == $this->view->user->y_hobbit) {
 				$this->view->boutiquerEchoppeOk = true;
 				$idEchoppe = $e["id_echoppe"];
 				break;
 			}
 		}
-		
+
 		if ($this->view->boutiquerEchoppeOk == false) {
 			return;
 		}
@@ -63,26 +63,26 @@ abstract class Bral_Competences_Boutiquer extends Bral_Competences_Competence {
 		if ($this->view->boutiquerEchoppeOk == false) {
 			throw new Zend_Exception(get_class($this)." Boutiquer interdit ");
 		}
-		
+
 		$details = "[h".$this->view->user->id_hobbit."] a boutiqué";
 		$id_type = $this->view->config->game->evenements->type->competence;
 		$this->setDetailsEvenement($details, $id_type);
 		$this->setEvenementQueSurOkJet1(false);
 
 		$this->calculBoutiquer();
-		
+
 		$this->calculPx();
 		$this->calculBalanceFaim();
 		$this->majHobbit();
 	}
-	
+
 	private function calculBoutiquer() {
-		
+
 		$this->view->ameliorationCompetence = false;
 		$this->view->tabCompetencesAmeliorees = null;
-		
+
 		$this->view->boutiquerMetierCourant = false;
-		
+
 		Zend_Loader::loadClass("HobbitsMetiers");
 		$hobbitsMetiersTable = new HobbitsMetiers();
 		$hobbitsMetierRowset = $hobbitsMetiersTable->findMetiersByHobbitId($this->view->user->id_hobbit);
@@ -94,10 +94,11 @@ abstract class Bral_Competences_Boutiquer extends Bral_Competences_Competence {
 				}
 			}
 		}
-		
+
 		$de10 = Bral_Util_De::get_1d10();
 		$deCompetence = 0;
-		
+
+		$de10 = 10;
 		if ($de10 >= 8 && $this->view->boutiquerMetierCourant) {
 			$this->view->ameliorationCompetence = true;
 			$deCompetence = Bral_Util_De::get_1d3();
@@ -109,20 +110,20 @@ abstract class Bral_Competences_Boutiquer extends Bral_Competences_Competence {
 				$this->updateCompetenceDb($deCompetence, $this->boutiquerMetier, 4);
 			}
 		}
-		
+
 		$this->view->deCompetence = $deCompetence;
-		
+
 		$petitEquipementTable = new PetitEquipement();
-		
+
 		$equipements = $petitEquipementTable->findByIdMetier($this->competence["id_fk_metier_competence"]);
 		if (count($equipements) < 6) {
 			throw new Zend_Exception(get_class($this)." Equipements invalides : idMetier:".$this->competence["id_fk_metier_competence"]);
 		}
 		$de6 = Bral_Util_De::get_1d6();
 		$petitEquipement = $equipements[$de6-1];
-		
+
 		$this->view->petitEquipement = $petitEquipement;
-		
+
 		$message = "";
 		$nbCastars = 0;
 		switch($de6) {
@@ -156,7 +157,7 @@ abstract class Bral_Competences_Boutiquer extends Bral_Competences_Competence {
 		}
 		$this->view->nbCastarsGagnes = $nbCastars;
 		$this->view->message = $message;
-		
+
 		$echoppeTable = new Echoppe();
 		$data = array(
 			'id_echoppe' => $this->idEchoppe,
@@ -164,35 +165,37 @@ abstract class Bral_Competences_Boutiquer extends Bral_Competences_Competence {
 		);
 		$echoppeTable->insertOrUpdate($data);
 	}
-	
+
 	private function updateCompetenceDb($deCompetence, $nomSystemeCompetence, $nbPa = -1) {
 		$hobbitsCompetencesTable = new HobbitsCompetences();
-		
+
 		if ($nbPa == -1) {
 			$competences = $hobbitsCompetencesTable->findByIdHobbitAndNomSysteme($this->view->user->id_hobbit, $nomSystemeCompetence);
 		} else {
 			$competences = $hobbitsCompetencesTable->findByIdHobbitAndNbPaAndNomSystemeMetier($this->view->user->id_hobbit, $nbPa, $this->boutiquerMetier);
 		}
-		
+
 		if ($competences == null || count($competences) == 0) {
 			throw new Zend_Exception(get_class($this)." Competences invalides :".$this->view->user->id_hobbit.",".$nbPa.",".$nomSystemeCompetence);
 		}
-		
+
 		$tabCompetencesAmeliorees = null;
-		
+
 		foreach ($competences as $c) {
-			$pourcentage = $c["pourcentage_hcomp"] + $deCompetence;
-			if ($pourcentage > $c["pourcentage_max_competence"]) { // % comp maximum
-				$pourcentage = $c["pourcentage_max_competence"];
+			if (("produire".$this->boutiquerMetier != $c["nom_systeme_competence"] && $nbPa != -1) || $nbPa == -1) {
+				$pourcentage = $c["pourcentage_hcomp"] + $deCompetence;
+				if ($pourcentage > $c["pourcentage_max_competence"]) { // % comp maximum
+					$pourcentage = $c["pourcentage_max_competence"];
+				}
+				$data = array('pourcentage_hcomp' => $pourcentage);
+				$where = array("id_fk_competence_hcomp = ".$c["id_fk_competence_hcomp"]." AND id_fk_hobbit_hcomp = ".$this->view->user->id_hobbit);
+				$hobbitsCompetencesTable->update($data, $where);
+				$tabCompetencesAmeliorees[] = $c;
 			}
-			$data = array('pourcentage_hcomp' => $pourcentage);
-			$where = array("id_fk_competence_hcomp = ".$c["id_fk_competence_hcomp"]." AND id_fk_hobbit_hcomp = ".$this->view->user->id_hobbit);
-			$hobbitsCompetencesTable->update($data, $where);
-			$tabCompetencesAmeliorees[] = $c;
 		}
 		$this->view->tabCompetencesAmeliorees = $tabCompetencesAmeliorees;
 	}
-	
+
 	public function getIdEchoppeCourante() {
 		if (isset($this->idEchoppe)) {
 			return $this->idEchoppe;
@@ -200,7 +203,7 @@ abstract class Bral_Competences_Boutiquer extends Bral_Competences_Competence {
 			return false;
 		}
 	}
-	
+
 	function getListBoxRefresh() {
 		return $this->constructListBoxRefresh(array("box_competences_metiers", "box_echoppes"));
 	}
