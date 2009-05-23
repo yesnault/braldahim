6660df58a440b0e702daf9bea6e37a5b49232cc4
BouleHondevice <boule.hondevice@braldahim.com>
2009-05-23 15:56:03 +0000

    Correction #445
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@1635 8885cbc4-9223-0410-882a-37b6fa6ce781

--- braldahim-site/library/Bral/Util/Poids.php
+++ braldahim-site/library/Bral/Util/Poids.php
@@ -12,21 +12,24 @@
  */
 class Bral_Util_Poids {
 
-	 const POIDS_CASTARS = 0.001;
-	 const POIDS_PEAU = 0.4;
-	 const POIDS_VIANDE = 0.4;
-	 const POIDS_VIANDE_PREPAREE = 0.3;
-	 const POIDS_CUIR = 0.4;
-	 const POIDS_FOURRURE = 0.4;
-	 const POIDS_PLANCHE = 3;
-	 const POIDS_RONDIN = 5;
-	 const POIDS_RUNE = 0.05;
-	 const POIDS_POTION = 0.3;
-	 const POIDS_MINERAI = 0.4;
-	 const POIDS_LINGOT = 0.5;
-	 const POIDS_PARTIE_PLANTE_BRUTE = 0.002;
-	 const POIDS_PARTIE_PLANTE_PREPAREE = 0.003;
-	 const POIDS_MUNITION = 0.04;
+	const POIDS_PEAU = 0.4;
+	const POIDS_VIANDE = 0.4;
+
+	const POIDS_VIANDE_PREPAREE = 0.3;
+	const POIDS_RATION = 0.4;
+	const POIDS_ALIMENT = 0.4;
+	const POIDS_CUIR = 0.4;
+	const POIDS_FOURRURE = 0.4;
+	const POIDS_PLANCHE = 3;
+	const POIDS_RONDIN = 4;
+	const POIDS_RUNE = 0.05;
+	const POIDS_POTION = 0.3;
+	const POIDS_MINERAI = 0.6;
+	const POIDS_LINGOT = 1;
+	const POIDS_PARTIE_PLANTE_BRUTE = 0.002;
+	const POIDS_PARTIE_PLANTE_PREPAREE = 0.003;
+	const POIDS_MUNITION = 0.04;
+	const POIDS_TABAC = 0;
 	
 	function __construct() {
 	}
--- braldahim/library/Bral/Util/Poids.php
+++ braldahim/library/Bral/Util/Poids.php
@@ -24,11 +24,11 @@ class Bral_Util_Poids {
 	const POIDS_CUIR = 0.4;
 	const POIDS_FOURRURE = 0.4;
 	const POIDS_PLANCHE = 3;
-	const POIDS_RONDIN = 5;
+	const POIDS_RONDIN = 4;
 	const POIDS_RUNE = 0.05;
 	const POIDS_POTION = 0.3;
-	const POIDS_MINERAI = 0.4;
-	const POIDS_LINGOT = 0.5;
+	const POIDS_MINERAI = 0.6;
+	const POIDS_LINGOT = 1;
 	const POIDS_PARTIE_PLANTE_BRUTE = 0.002;
 	const POIDS_PARTIE_PLANTE_PREPAREE = 0.003;
 	const POIDS_MUNITION = 0.04;
