7ddcc387ad1367a3b0ff07c8538f0b977489ca91
BouleHondevice <boule.hondevice@braldahim.com>
2009-05-31 22:05:37 +0000

    Avancement Hôtel
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@1681 8885cbc4-9223-0410-882a-37b6fa6ce781

--- braldahim/library/Bral/Hotel/Voir.php
+++ braldahim/library/Bral/Hotel/Voir.php
@@ -24,6 +24,7 @@ class Bral_Hotel_Voir extends Bral_Hotel_Hotel {
 	}
 	
 	function render() {
+		$this->prepareMenu();
 		return $this->view->render("hotel/voir.phtml");
 	}
 
@@ -37,5 +38,36 @@ class Bral_Hotel_Voir extends Bral_Hotel_Hotel {
 	}
 
 	function getListBoxRefresh() {
+	}
+	
+	private function prepareMenu() {
+		$tabMenu = null;
+		
+		$tabMenu[] = $this->prepareMenuEquipement();
+		$tabMenu[] = array("titre" => "Matières Premières");
+		$tabMenu[] = array("titre" => "Matières Transformées");
+		$tabMenu[] = array("titre" => "Matériels");
+		$tabMenu[] = array("titre" => "Potions");
+		$tabMenu[] = array("titre" => "Runes");
+		
+		$this->view->menuRecherche = $tabMenu;
+	}
+	
+	private function prepareMenuEquipement() {
+		$retour = array("titre" => "Équipements");
+		
+		Zend_Loader::loadClass("TypeEmplacement");
+		$typeEmplacementTable = new TypeEmplacement();
+		$typesEmplacements = $typeEmplacementTable->fetchAll();
+		$typesEmplacements = $typesEmplacements->toArray();
+		
+		$elements = null;
+		foreach($typesEmplacements as $e) {
+			$elements[] = array('nom' => $e["nom_type_emplacement"], "table" => "TypeEmplacement", "id_element" => $e["id_type_emplacement"]);
+		}
+		
+		$retour["elements"] = $elements;
+		
+		return $retour;
 	}
 }
