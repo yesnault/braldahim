f2195c73470ad8695abae19795f56b3e6f5204a2
BouleHondevice <boule.hondevice@braldahim.com>
2009-06-13 17:51:06 +0000

    Marcher - 0PA
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@1739 8885cbc4-9223-0410-882a-37b6fa6ce781

--- braldahim/library/Bral/Competences/Marcher.php
+++ braldahim/library/Bral/Competences/Marcher.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This file is part of Braldahim, under Gnu Public Licence v3. 
+ * This file is part of Braldahim, under Gnu Public Licence v3.
  * See licence.txt or http://www.gnu.org/licenses/gpl-3.0.html
  *
  * $Id$
@@ -11,16 +11,21 @@
  * $LastChangedBy$
  */
 class Bral_Competences_Marcher extends Bral_Competences_Competence {
-	
+
 	function prepareCommun() {
-		Zend_Loader::loadClass('Bral_Util_Marcher'); 
-		Zend_Loader::loadClass('Bral_Util_Quete'); 
-		
+
+		if ($this->view->assezDePa == false) {
+			return;
+		}
+
+		Zend_Loader::loadClass('Bral_Util_Marcher');
+		Zend_Loader::loadClass('Bral_Util_Quete');
+
 		$utilMarcher = new Bral_Util_Marcher();
-		
+
 		$selection = $this->request->get("valeur_1"); // si l'on vient de la vue (clic sur l'icone marcher)
 		$calcul = $utilMarcher->calcul($this->view->user, $selection);
-		
+
 		$this->view->effetMot = $calcul["effetMot"];
 		$this->view->assezDePa  = $calcul["assezDePa"];
 		$this->view->nb_cases = $calcul["nb_cases"];
@@ -31,54 +36,56 @@ class Bral_Competences_Marcher extends Bral_Competences_Competence {
 		$this->view->marcherPossible = $calcul["marcherPossible"];
 		$this->view->estEngage = $calcul["estEngage"];
 		$this->view->estSurRoute = $calcul["estSurRoute"];
-		
+
 		$this->view->x_min = $calcul["x_min"];
 		$this->view->x_max = $calcul["x_max"];
 		$this->view->y_min = $calcul["y_min"];
 		$this->view->y_max = $calcul["y_max"];
 	}
-	
+
 	function calculNbPa() {
-		// fait dans UtilMarcher	
+		// fait dans UtilMarcher
 	}
-	
+
 	function prepareFormulaire() {
+	}
+
+	function prepareResultat() {
+
 		if ($this->view->assezDePa == false) {
 			return;
 		}
-	}
-	
-	function prepareResultat() {
+
 		$x_y = $this->request->get("valeur_1");
 		list ($offset_x, $offset_y) = split("h", $x_y);
-		
+
 		if ($offset_x < -$this->view->nb_cases || $offset_x > $this->view->nb_cases) {
 			throw new Zend_Exception(get_class($this)." Deplacement X impossible : ".$offset_x);
 		}
-		
+
 		if ($offset_y < -$this->view->nb_cases || $offset_y > $this->view->nb_cases) {
 			throw new Zend_Exception(get_class($this)." Deplacement Y impossible : ".$offset_y);
 		}
-		
+
 		if ($this->tableauValidation[$offset_x][$offset_y] !== true) {
 			throw new Zend_Exception(get_class($this)." Deplacement XY impossible : ".$offset_x.$offset_y);
 		}
-		
+
 		$this->view->user->x_hobbit = $this->view->user->x_hobbit + $offset_x;
 		$this->view->user->y_hobbit = $this->view->user->y_hobbit + $offset_y;
-		
+
 		$id_type = $this->view->config->game->evenements->type->deplacement;
 		$details = "[h".$this->view->user->id_hobbit."] a marché";
 		$this->setDetailsEvenement($details, $id_type);
 		$this->setEvenementQueSurOkJet1(false);
-		
+
 		$this->view->estQueteEvenement = Bral_Util_Quete::etapeMarcher($this->view->user);
-		
+
 		$this->calculBalanceFaim();
 		$this->calculFinMatchSoule();
 		$this->majHobbit();
 	}
-	
+
 	function getListBoxRefresh() {
 		$tab = array("box_vue", "box_lieu", "box_echoppes");
 		if ($this->view->user->est_soule_hobbit == "oui") {
@@ -86,5 +93,5 @@ class Bral_Competences_Marcher extends Bral_Competences_Competence {
 		}
 		return $this->constructListBoxRefresh($tab);
 	}
-	
+
 }
