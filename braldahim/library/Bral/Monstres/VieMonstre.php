e2ced2aba049de8086bd7936a3123d13b8b13d2f
BouleHondevice <boule.hondevice@braldahim.com>
2009-08-12 07:21:54 +0000

    Correction esquive sans floor
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@1892 8885cbc4-9223-0410-882a-37b6fa6ce781

--- braldahim/library/Bral/Monstres/VieMonstre.php
+++ braldahim/library/Bral/Monstres/VieMonstre.php
@@ -255,7 +255,7 @@ Consultez vos événements pour plus de détails.";
 			}
 
 		} else if ($jetCible/2 < $jetAttaquant) {
-			$cible["agilite_bm_hobbit"] = $cible["agilite_bm_hobbit"] - floor($cible["niveau_hobbit"]);
+			$cible["agilite_bm_hobbit"] = $cible["agilite_bm_hobbit"] - $cible["niveau_hobbit"];
 			$this->updateCible($cible);
 			$id_type_evenement = self::$config->game->evenements->type->attaquer;
 			$details = "[m".$this->monstre["id_monstre"]."] a attaqué le hobbit [h".$cible["id_hobbit"]."] qui a esquivé l'attaque";
