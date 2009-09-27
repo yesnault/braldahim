7de1106f05bef3a50dadd3310f88d0d82b378966
BouleHondevice <boule.hondevice@braldahim.com>
2009-09-27 17:31:10 +0000

    Avancement #315
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@2046 8885cbc4-9223-0410-882a-37b6fa6ce781

--- braldahim/library/Bral/Administrationajax/Positionnervue.php
+++ braldahim/library/Bral/Administrationajax/Positionnervue.php
@@ -31,10 +31,11 @@ class Bral_Administrationajax_Positionnervue extends Bral_Administrationajax_Adm
 		
 		$idVille = $this->request->get("id_ville");
 		$idLieu = $this->request->get("id_lieu");
-		$xyPosition = $this->request->get("xy_position");
+		$xyzPosition = $this->request->get("xyz_position");
 		
 		$x = $this->view->user->x_hobbit;
 		$y = $this->view->user->y_hobbit;
+		$z = $this->view->user->z_hobbit;
 		
 		if ($idVille != null) {
 			Bral_Util_Controle::getValeurIntVerif($idVille);
@@ -43,6 +44,7 @@ class Bral_Administrationajax_Positionnervue extends Bral_Administrationajax_Adm
 			$ville = $villeTable->findById($idVille);
 			$x = $ville->x_min_ville + floor(($ville->x_max_ville - $ville->x_min_ville) / 2);
 			$y = $ville->y_min_ville + floor(($ville->y_max_ville - $ville->y_min_ville) / 2);
+			$z = 0;
 				
 		} elseif ($idLieu != null) {
 			Bral_Util_Controle::getValeurIntVerif($idLieu);
@@ -51,14 +53,17 @@ class Bral_Administrationajax_Positionnervue extends Bral_Administrationajax_Adm
 			$lieu = $lieuTable->findById($idLieu);
 			$x = $lieu->x_lieu;
 			$y = $lieu->y_lieu;
-		} elseif ($xyPosition != null) {
-			list ($x, $y) = split("h", $xyPosition);
+			$y = $lieu->z_lieu;
+		} elseif ($xyzPosition != null) {
+			list ($x, $y, $z) = split("h", $xyzPosition);
 			Bral_Util_Controle::getValeurIntVerif($x);
 			Bral_Util_Controle::getValeurIntVerif($y);
+			Bral_Util_Controle::getValeurIntVerif($z);
 		}
 		
 		Zend_Auth::getInstance()->getIdentity()->administrationvueDonnees["x_position"] = $x;
 		Zend_Auth::getInstance()->getIdentity()->administrationvueDonnees["y_position"] = $y;
+		Zend_Auth::getInstance()->getIdentity()->administrationvueDonnees["z_position"] = $z;
 	}
 
 	function getListBoxRefresh() {
