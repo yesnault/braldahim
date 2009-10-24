adae4f7c9973db40b9aba2de56e38e2b4ad89ac6
BouleHondevice <boule.hondevice@braldahim.com>
2009-10-24 14:05:40 +0000

    Un peu de ménage
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@2110 8885cbc4-9223-0410-882a-37b6fa6ce781

--- braldahim/application/models/Partage.php
+++ braldahim/application/models/Partage.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This file is part of Braldahim, under Gnu Public Licence v3. 
+ * This file is part of Braldahim, under Gnu Public Licence v3.
  * See licence.txt or http://www.gnu.org/licenses/gpl-3.0.html
  *
  * $Id: $
@@ -13,4 +13,15 @@
 class Partage extends Zend_Db_Table {
 	protected $_name = 'partage';
 	protected $_primary = 'id_partage';
+
+	function findByIdHobbit($idHobbit) {
+		$where = "id_fk_hobbit_declarant_partage = ".intval($idHobbit). " OR id_fk_hobbit_declare_partage=".intval($idHobbit);
+		
+		$db = $this->getAdapter();
+		$select = $db->select();
+		$select->from('partage', '*')
+		->where($where);
+		$sql = $select->__toString();
+		return $db->fetchAll($sql);
+	}
 }
