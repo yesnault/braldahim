53fa8e85143a702e18a98d83e9b2320a3b43d32f
BouleHondevice <boule.hondevice@braldahim.com>
2009-11-01 20:49:17 +0000

    Avancement #315
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@2145 8885cbc4-9223-0410-882a-37b6fa6ce781

---
+++ braldahim/library/Bral/Lieux/Escaliers.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This file is part of Braldahim, under Gnu Public Licence v3. 
+ * See licence.txt or http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * $Id: $
+ * $Author: $
+ * $LastChangedDate: $
+ * $LastChangedRevision: $
+ * $LastChangedBy: $
+ */
+class Bral_Lieux_Escaliers extends Bral_Lieux_Lieu {
+
+	function prepareCommun() {
+		Zend_Loader::loadClass("Lieu");
+	}
+
+	function prepareFormulaire() {
+	}
+
+	function prepareResultat() {
+		$this->view->user->z_hobbit = $this->view->user->z_hobbit + 1;
+		$this->majHobbit();
+	}
+
+	function getListBoxRefresh() {
+		return $this->constructListBoxRefresh(array("box_vue", "box_lieu"));
+	}
+}
