841d682ee103d6df12675eacc217ec399e0291c4
BouleHondevice <boule.hondevice@braldahim.com>
2009-12-16 23:59:13 +0000

    gain castars
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@2254 8885cbc4-9223-0410-882a-37b6fa6ce781

--- braldahim/library/Bral/Util/Niveau.php
+++ braldahim/library/Bral/Util/Niveau.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This file is part of Braldahim, under Gnu Public Licence v3. 
+ * This file is part of Braldahim, under Gnu Public Licence v3.
  * See licence.txt or http://www.gnu.org/licenses/gpl-3.0.html
  *
  * $Id$
@@ -11,10 +11,10 @@
  * $LastChangedBy$
  */
 class Bral_Util_Niveau {
-	
+
 	private function __construct() {
 	}
-	
+
 	/**
 	 * Le niveau suivant est calculé à partir d'un certain nombre de px perso
 	 * qui doit être >= à :
@@ -25,9 +25,9 @@ class Bral_Util_Niveau {
 		if ($changeNiveau == null) {
 			$changeNiveau = false;
 		}
-		
+
 		$niveauDiv10Precedent = floor($hobbit->niveau_hobbit / 10);
-		
+
 		$niveauSuivantPx = ($hobbit->niveau_hobbit + 1) * 5;
 		if ($hobbit->px_perso_hobbit >= $niveauSuivantPx) {
 			$hobbit->px_perso_hobbit = $hobbit->px_perso_hobbit - $niveauSuivantPx;
@@ -35,11 +35,13 @@ class Bral_Util_Niveau {
 			$hobbit->pi_cumul_hobbit = $hobbit->pi_cumul_hobbit + $niveauSuivantPx;
 			$hobbit->pi_hobbit = $hobbit->pi_hobbit + $niveauSuivantPx;
 			$changeNiveau = true;
-			
+
 			$niveauDiv10Actuel = floor($hobbit->niveau_hobbit / 10);
 			if ($niveauDiv10Precedent != $niveauDiv10Actuel) {
 				self::calculTitre(&$hobbit);
 			}
+
+			self::gainCastars($hobbit);
 		}
 
 		$niveauSuivantPx = ($hobbit->niveau_hobbit + 1) * 5;
@@ -48,33 +50,50 @@ class Bral_Util_Niveau {
 		}
 		return $changeNiveau;
 	}
-	
+
 	private static function calculTitre(&$hobbit) {
 		Zend_Loader::loadClass("TypeTitre");
 		Zend_Loader::loadClass("HobbitsTitres");
 		Zend_Loader::loadClass("Bral_Util_Titre");
-		
+
 		$idTitre = Bral_Util_De::get_1d4();
-		
+
 		$data = array(
 			'id_fk_hobbit_htitre' => $hobbit->id_hobbit,
 			'id_fk_type_htitre' => $idTitre,
 			'niveau_acquis_htitre' => floor($hobbit->niveau_hobbit / 10) * 10,
 			'date_acquis_htitre' => date("Y-m-d"),
 		);
-		
+
 		$hobbitsTitres = new HobbitsTitres();
 		$hobbitsTitres->insert($data);
-		
+
 		$typeTitre = new TypeTitre();
 		$typeTitreRowset = $typeTitre->findById($idTitre);
-		
+
 		if ($hobbit->sexe_hobbit == "feminin") {
 			$hobbit->titre_courant_hobbit = $typeTitreRowset->nom_feminin_type_titre;
 		} else {
 			$hobbit->titre_courant_hobbit = $typeTitreRowset->nom_masculin_type_titre;
 		}
-		
+
 		Bral_Util_Titre::calculNouveauTitre(&$hobbit, $typeTitreRowset);
+	}
+
+	// jusqu'au niveau 10 inclut, niveau*50 castars sont placés à la banque
+	private static function gainCastars($hobbit) {
+		
+		if ($hobbit->niveau_hobbit < 11) {
+			$nbCastars = $hobbit->niveau_hobbit * 50;
+
+			Zend_Loader::loadClass("Coffre");
+			$coffreTable = new Coffre();
+			$data = array(
+				"quantite_castar_coffre" => $nbCastars,
+				"id_fk_hobbit_coffre" => $hobbit->id_hobbit,
+			);
+			$coffreTable->insertOrUpdate($data);
+		}
+
 	}
 }
