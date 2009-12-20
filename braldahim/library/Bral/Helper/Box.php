64311fc59af66194d3b4e4557a7038b1ed5ef442
BouleHondevice <boule.hondevice@braldahim.com>
2009-12-20 19:16:27 +0000

    Amélio vue
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@2270 8885cbc4-9223-0410-882a-37b6fa6ce781

--- braldahim/library/Bral/Helper/Box.php
+++ braldahim/library/Bral/Helper/Box.php
@@ -12,13 +12,11 @@
  */
 class Bral_Helper_Box {
 	
-    public static function haut() {
-		$retour = '<b class="xb_t1"></b><b class="xb_t2"></b><b class="xb_t3"></b><b class="xb_t4"></b><b class="xb_t5"></b><b class="xb_t6"></b><b class="xb_t7"></b>';
-		return $retour;
+    public static function haut($style="") {
+		return '<b class="xb_t1"></b><b class="xb_t2"></b><b class="xb_t3'.$style.'"></b><b class="xb_t4'.$style.'"></b><b class="xb_t5'.$style.'"></b><b class="xb_t6'.$style.'"></b><b class="xb_t7'.$style.'"></b>';
     }
     
-    public static function bas() {
-		$retour = '<b class="xb_b7"></b><b class="xb_b6"></b><b class="xb_b5"></b><b class="xb_b4"></b><b class="xb_b3"></b><b class="xb_b2"></b><b class="xb_b1"></b>';
-		return $retour;
+    public static function bas($style="") {
+		return '<b class="xb_b7'.$style.'"></b><b class="xb_b6'.$style.'"></b><b class="xb_b5'.$style.'"></b><b class="xb_b4'.$style.'"></b><b class="xb_b3'.$style.'"></b><b class="xb_b2"></b><b class="xb_b1"></b>';
     }
 }
