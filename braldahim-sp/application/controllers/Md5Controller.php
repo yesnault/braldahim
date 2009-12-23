d5347050318a745dfe2802a5ba0fd9bb58145909
BouleHondevice <boule.hondevice@braldahim.com>
2009-12-23 10:54:47 +0000

    Avancement #607
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@2286 8885cbc4-9223-0410-882a-37b6fa6ce781

---
+++ braldahim-sp/application/controllers/Md5Controller.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * This file is part of Braldahim, under Gnu Public Licence v3.
+ * See licence.txt or http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * $Id: $
+ * $Author: $
+ * $LastChangedDate: $
+ * $LastChangedRevision: $
+ * $LastChangedBy: $
+ */
+class Md5Controller extends Zend_Controller_Action {
+
+	function init() {
+		$this->initView();
+		$this->view->config = Zend_Registry::get('config');
+	}
+
+	function indexAction() {
+		$this->view->md5_value = "";
+		if ($this->_request->get("md5_source") != "") {
+			$this->view->md5_source = $this->_request->get("md5_source");
+			$this->view->md5_value = md5($this->_request->get("md5_source"));
+		}
+		$this->render();
+	}
+
+	function md5Action() {
+		$this->render();
+	}
+}
