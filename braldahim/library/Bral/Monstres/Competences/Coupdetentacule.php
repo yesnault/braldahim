e77170e3b84c60c03acee218f2a1196acea1b1bd
BouleHondevice <boule.hondevice@braldahim.com>
2010-01-10 13:21:29 +0000

    Avancement #618
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@2341 8885cbc4-9223-0410-882a-37b6fa6ce781

---
+++ braldahim/library/Bral/Monstres/Competences/Coupdetentacule.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * This file is part of Braldahim, under Gnu Public Licence v3.
+ * See licence.txt or http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * $Id: $
+ * $Author: $
+ * $LastChangedDate: $
+ * $LastChangedRevision: $
+ * $LastChangedBy: $
+ */
+class Bral_Monstres_Competences_Coupdetentacule extends Bral_Monstres_Competences_Attaque {
+
+	public function calculJetAttaque(){}
+	public function calculDegat($estCritique){}
+
+	public function actionSpecifique() {
+		Bral_Util_Log::viemonstres()->trace(get_class($this)."  - actionSpecifique - enter");
+
+		Zend_Loader::loadClass("Bral_Util_Effets");
+
+		$malus = floor($this->monstre["niveau_monstre"] / 4);
+		$nbTours = Bral_Util_De::get_1d3() + 1;
+
+		$jetMonstre = Bral_Util_De::getLanceDe6(self::$config->game->base_sagesse + $this->monstre["sagesse_base_monstre"]);
+		$jetMonstre = $jetMonstre + $this->monstre["sagesse_bm_monstre"];
+
+		$jetHobbit = Bral_Util_De::getLanceDe6(self::$config->game->base_vigueur + $this->cible["vigueur_base_hobbit"]);
+		$jetHobbit = $jetHobbit + $this->cible["vigueur_bm_hobbit"] + $this->cible["vigueur_bbdf_hobbit"];
+
+		if ($jetHobbit > $jetMonstre) {
+			$malus = floor($malus / 2);
+			$nbTours = 1;
+		}
+
+		Bral_Util_Effets::ajouteEtAppliqueEffetHobbit($this->cible["id_hobbit"], Bral_Util_Effets::CARACT_PV, Bral_Util_Effets::TYPE_MALUS, $nbTours, $malus, "Coup de tentacule empoisonnée");
+		$this->majEvenement($this->cible, $malus, $nbTours, $jetMonstre, $jetHobbit);
+
+		Bral_Util_Log::viemonstres()->trace(get_class($this)."  - actionSpecifique - exit");
+		return null;
+	}
+
+	private function majEvenement($hobbit, $malus, $nbTours, $jetMonstre, $jetHobbit) {
+		Bral_Util_Log::viemonstres()->trace(get_class($this)."  - majEvenement - enter");
+		$idTypeEvenement = self::$config->game->evenements->type->attaquer;
+		$details = "[m".$this->monstre["id_monstre"]."] donne un coup de tentacule empoisonnée sur le hobbit [h".$hobbit["id_hobbit"]."]";
+		$detailsBot = $this->getDetailsBot($malus, $nbTours, $jetMonstre, $jetHobbit);
+		Bral_Util_Evenement::majEvenementsFromVieMonstre($hobbit["id_hobbit"], $this->monstre["id_monstre"], $idTypeEvenement, $details, $detailsBot, $hobbit["niveau_hobbit"], $this->view);
+		Bral_Util_Log::viemonstres()->trace(get_class($this)."  - majEvenement - exit");
+	}
+
+	protected function getDetailsBot($malus, $nbTours, $jetMonstre, $jetHobbit) {
+		Bral_Util_Log::viemonstres()->trace(get_class($this)."  - getDetailsBot - enter");
+		$retour = "";
+		$retour .= $this->monstre["nom_type_monstre"] ." (".$this->monstre["id_monstre"].") vous a donné un coup de tentacule empoisonnée :";
+		$retour .= PHP_EOL."Jet du Monstre (jet de sagesse) : ".$jetMonstre;
+		$retour .= PHP_EOL."Jet de résistance (jet de vigueur) : ".$jetHobbit;
+		if ($jetHobbit > $jetMonstre) {
+			$retour .= PHP_EOL."Vous avez résisté au coup, le malus est divisé par 2 et sur 1 tour seulement.";
+		} else {
+			$retour .= PHP_EOL."Vous n'avez pas résisté au coup.";
+		}
+		$retour .= PHP_EOL."Nombre de Points de vie en moins : -".$malus." PV";
+		$retour .= PHP_EOL."Nombre de tours : ".$nbTours;
+		Bral_Util_Log::viemonstres()->trace(get_class($this)."  - getDetailsBot - exit");
+		return $retour;
+	}
+}
