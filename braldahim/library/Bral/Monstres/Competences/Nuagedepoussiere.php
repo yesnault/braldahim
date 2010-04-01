0de2a3bec8556fc45b17150fbc63dc4b226c50f3
BouleHondevice <boule.hondevice@braldahim.com>
2010-04-01 17:25:01 +0000

    Correction msg
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@2547 8885cbc4-9223-0410-882a-37b6fa6ce781

--- braldahim/library/Bral/Monstres/Competences/Nuagedepoussiere.php
+++ braldahim/library/Bral/Monstres/Competences/Nuagedepoussiere.php
@@ -44,7 +44,7 @@ class Bral_Monstres_Competences_Nuagedepoussiere extends Bral_Monstres_Competenc
 	private function majEvenement($hobbit, $malus, $nbTours, $jetMonstre, $jetHobbit) {
 		Bral_Util_Log::viemonstres()->trace(get_class($this)."  - majEvenement - enter");
 		$idTypeEvenement = self::$config->game->evenements->type->attaquer;
-		$details = "[m".$this->monstre["id_monstre"]."] crée un nuage de poussière, le hobbit [h".$hobbit["id_hobbit"]."]";
+		$details = "[m".$this->monstre["id_monstre"]."] crée un nuage de poussière, le hobbit [h".$hobbit["id_hobbit"]."] est influencé";
 		$detailsBot = $this->getDetailsBot($malus, $nbTours, $jetMonstre, $jetHobbit);
 		Bral_Util_Evenement::majEvenementsFromVieMonstre($hobbit["id_hobbit"], $this->monstre["id_monstre"], $idTypeEvenement, $details, $detailsBot, $hobbit["niveau_hobbit"], $this->view);
 		Bral_Util_Log::viemonstres()->trace(get_class($this)."  - majEvenement - exit");
