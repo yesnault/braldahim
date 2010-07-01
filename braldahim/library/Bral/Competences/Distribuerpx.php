b6c0c726643d2dddfb8b2563925318407371255c
BouleHondevice <boule.hondevice@braldahim.com>
2010-07-01 18:12:00 +0000

    Correction exception
    git-svn-id: https://braldahim.googlecode.com/svn/trunk@2780 8885cbc4-9223-0410-882a-37b6fa6ce781

--- braldahim/library/Bral/Competences/Distribuerpx.php
+++ braldahim/library/Bral/Competences/Distribuerpx.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This file is part of Braldahim, under Gnu Public Licence v3. 
+ * This file is part of Braldahim, under Gnu Public Licence v3.
  * See licence.txt or http://www.gnu.org/licenses/gpl-3.0.html
  *
  * $Id$
@@ -23,14 +23,20 @@ class Bral_Competences_Distribuerpx extends Bral_Competences_Competence {
 		$vue = Bral_Util_Commun::getVueBase($this->view->user->x_braldun, $this->view->user->y_braldun, $this->view->user->z_braldun) + $this->view->user->vue_bm_braldun;
 		$bralduns = $braldunTable->findLesPlusProches($this->view->user->x_braldun, $this->view->user->y_braldun, $this->view->user->z_braldun, $vue, $this->view->config->game->competence->distribuerpx->nb_max_braldun);
 
+		$tabBralduns = null;
+		
 		foreach($bralduns as $h) {
-			if ($h["id_braldun"] == $this->view->user->id_braldun) {
-				$nom = " Vous-Même : ".$h["prenom_braldun"]. " ". $h["nom_braldun"];
-			}
-			$tabBralduns[] = array("id_braldun" => $h["id_braldun"],
+			if ($this->view->user->points_gredin_braldun <= 0 || // si l'on est pas gredin
+			($this->view->user->points_gredin_braldun > 0 && $h["points_gredin_braldun"] > 0)) { // si l'on est gredin et que le destinataire également
+				if ($h["id_braldun"] == $this->view->user->id_braldun) {
+					$nom = " Vous-Même : ".$h["prenom_braldun"]. " ". $h["nom_braldun"];
+				}
+				$tabBralduns[] = array("id_braldun" => $h["id_braldun"],
 				 "nom_braldun" => $h["nom_braldun"], 
 				 "prenom_braldun" => $h["prenom_braldun"],
-				 "niveau_braldun" => $h["niveau_braldun"]);
+				 "niveau_braldun" => $h["niveau_braldun"]);		
+			}
+
 		}
 
 		if (count($tabBralduns) >= $this->view->config->game->competence->distribuerpx->nb_max_braldun) {
@@ -77,7 +83,7 @@ class Bral_Competences_Distribuerpx extends Bral_Competences_Competence {
 		if ($total_distribution > $this->view->user->px_commun_braldun) {
 			throw new Zend_Exception(get_class($this)." Total trop eleve:".$total_distribution. " c=".$this->view->user->px_commun_braldun);
 		}
-		
+
 		$moisEnCours  = mktime(0, 0, 0, date("m"), 2, date("Y"));
 		$dataStats["mois_stats_experience"] = date("Y-m-d", $moisEnCours);
 		$statsExperience = new StatsExperience();
@@ -95,12 +101,12 @@ class Bral_Competences_Distribuerpx extends Bral_Competences_Competence {
 			);
 			$where = "id_braldun=".$t["id_braldun"];
 			$braldunTable->update($data, $where);
-			
+
 			$dataStats["nb_px_perso_gagnes_stats_experience"] = $t["px_recu"];
 			$dataStats["id_fk_braldun_stats_experience"] = $t["id_braldun"];
 			$dataStats["niveau_braldun_stats_experience"] = $t["niveau_braldun"];
 			$statsExperience->insertOrUpdate($dataStats);
-		
+
 			if ($t["id_braldun"] == $this->view->user->id_braldun) {
 				$this->view->user->px_perso_braldun = $braldun->px_perso_braldun;
 			}
@@ -111,16 +117,16 @@ class Bral_Competences_Distribuerpx extends Bral_Competences_Competence {
 			} else {
 				$tab["nom_braldun"] = $braldun->prenom_braldun. " " .$braldun->nom_braldun;
 			}
-			
+
 			$tab["nom_braldun_details"] = $braldun->prenom_braldun. " " .$braldun->nom_braldun;
-			
+
 			$tab["px_recu"] = $t["px_recu"];
 			$tabAffiche[] = $tab;
 
 			$id_type = $this->view->config->game->evenements->type->don;
 			$detailsD = "[b".$this->view->user->id_braldun."] a donné des PX à [b".$tab["id_braldun"]."]";
 			$detailsR = "[b".$tab["id_braldun"]."] a reçu des PX de la part de [b".$this->view->user->id_braldun."]";
-			
+
 			$detailDonneur = "Vous avez donné ".$tab["px_recu"]." PX à ".$tab["nom_braldun"]." (".$tab["id_braldun"].")";
 			$detailReceveur = "Vous avez reçu ".$tab["px_recu"]." PX de la part de ".$this->view->user->prenom_braldun ." ". $this->view->user->nom_braldun ." (".$this->view->user->id_braldun.")";
 			Bral_Util_Evenement::majEvenements($this->view->user->id_braldun, $id_type, $detailsD, $detailDonneur, $this->view->user->niveau_braldun);
@@ -133,7 +139,7 @@ class Bral_Competences_Distribuerpx extends Bral_Competences_Competence {
 
 		$this->view->tabAffiche = $tabAffiche;
 		$this->view->totalDistribution = $total_distribution;
-		
+
 		$this->majBraldun();
 	}
 
