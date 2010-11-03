1ff26056addc3ec5b65ccca33332029fd6416c23
BouleHondevice <boule.hondevice@braldahim.com>
2010-11-03 21:47:44 +0100

    Avancement

---
+++ braldahim/library/Bral/Monstres/Competences/Feutenebres.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This file is part of Braldahim, under Gnu Public Licence v3.
+ * See licence.txt or http://www.gnu.org/licenses/gpl-3.0.html
+ * Copyright: see http://www.braldahim.com/sources
+ */
+class Bral_Monstres_Competences_Feutenebres extends Bral_Monstres_Competences_Attaque {
+
+	public function calculJetAttaque(){}
+	public function calculDegat($estCritique){}
+
+	public function actionSpecifique() {
+	}
+
+	private function majEvenement($braldun, $malus, $nbTours, $jetMonstre, $jetBraldun) {
+	}
+
+	protected function getDetailsBot($malus, $nbTours, $jetMonstre, $jetBraldun) {
+	}
+}
