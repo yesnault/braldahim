2b0a37ab1e17534d42188c045f36e9eeb54f5ae1
BouleHondevice <boule.hondevice@braldahim.com>
2011-04-28 22:00:11 +0200

    Avancement #810

--- braldahim-site/public/scripts/braldahim/m_action-site-005.js
+++ braldahim-site/public/scripts/braldahim/m_action-site-005.js
@@ -498,5 +498,26 @@ $(document).ready(function(){
 			]
 		});
 	}
+	
+	if ($('#pageflip').exists()) {
+		$("#pageflip").hover(function() { //On hover...
+			$("#pageflip img , .msg_block").stop()
+				.animate({ //Animate and expand the image and the msg_block (Width + height)
+					width: '307px',
+					height: '319px'
+				}, 500);
+			} , function() {
+			$("#pageflip img").stop() //On hover out, go back to original size 50x52
+				.animate({
+					width: '50px',
+					height: '52px'
+				}, 220);
+			$(".msg_block").stop() //On hover out, go back to original size 50x50
+				.animate({
+					width: '50px',
+					height: '50px'
+				}, 200); //Note this one retracts a bit faster (to prevent glitching in IE)
+		});
+	}
 
 });
