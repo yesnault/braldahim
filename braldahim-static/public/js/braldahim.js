b8dbbb6a4ea7e0f8de547b50b2a52f706715d27b
BouleHondevice <boule.hondevice@braldahim.com>
2011-05-22 11:28:41 +0200

    Correction couleur form login. #810

--- braldahim-static/public/js/braldahim.js
+++ braldahim-static/public/js/braldahim.js
@@ -1564,5 +1564,15 @@ $(document).ready(function() {
 		messagerie();
 	}
 	
+	if (navigator.userAgent.toLowerCase().indexOf("chrome") >= 0) {
+		$(window).load(function(){
+		    $('input:-webkit-autofill').each(function(){
+		        var text = $(this).val();
+		        var name = $(this).attr('name');
+		        $(this).after(this.outerHTML).remove();
+		        $('input[name=' + name + ']').val(text);
+		    });
+		});}
+	
 });
 
