5344d9f418b00de49a2c73370a565d75ee77b73c
BouleHondevice <boule.hondevice@braldahim.com>
2011-06-13 01:20:30 +0200

    Fin correction alert

--- braldahim-static/public/js/braldahim.js
+++ braldahim-static/public/js/braldahim.js
@@ -1726,8 +1726,6 @@ $(document).ready(function() {
 		if ($('#box_warning').exists()) {
 			$('#box_warning').html("Firefox 3.x n'est plus supporté par Braldahim. Utilisez Firefox 4 minimum ou un autre navigateur ou vous aurez quelques problèmes d'affichage...");
 			$('#box_warning').show();
-		} else {
-			alert("Firefox 3.x n'est plus supporté par Braldahim. Utilisez Firefox 4 minimum ou un autre navigateur ou vous aurez quelques problèmes d'affichage...");
 		}
 	} else if ($.browser.ie == true && $.browser.version < '9.0') {
 		alert("Internet Explorer n'est plus supporté par Braldahim. Utilisez un autre navigateur");
