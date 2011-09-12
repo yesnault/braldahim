abc9395984cd81ca98a4c79f918fe8241c705475
BouleHondevice <boule.hondevice@braldahim.com>
2011-09-12 22:55:09 +0200

    Close #53

--- braldahim/library/Bral/Voir/Factory.php
+++ braldahim/library/Bral/Voir/Factory.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This file is part of Braldahim, under Gnu Public Licence v3. 
+ * This file is part of Braldahim, under Gnu Public Licence v3.
  * See licence.txt or http://www.gnu.org/licenses/gpl-3.0.html
  * Copyright: see http://www.braldahim.com/sources
  */
@@ -14,21 +14,21 @@ class Bral_Voir_Factory {
 		$section = $matches[2];
 		$construct = null;
 
-		$construct = "Bral_Voir_".Bral_Util_String::firstToUpper($section);
+		$construct = "Bral_Voir_" . Bral_Util_String::firstToUpper($section);
 		// verification que la classe existe.
 		try {
-			Zend_Loader::loadClass($construct);  
-	    } catch(Exception $e) {
-	  	  	throw new Zend_Exception("Bral_Voir_Factory Action invalide (classe): ".$construct);
-	    }
-	    
+			Zend_Loader::loadClass($construct);
+		} catch (Exception $e) {
+			throw new Zend_Exception("Bral_Voir_Factory Action invalide (classe): " . $construct);
+		}
+
 		if (class_exists($construct)) {
 			return new $construct ($request, $view);
 		} else {
-			throw new Zend_Exception("Bral_Voir_Factory Classe invalide: ".$construct);
+			throw new Zend_Exception("Bral_Voir_Factory Classe invalide: " . $construct);
 		}
 	}
-	
+
 	static function getCommunaute($request, $view) {
 		Zend_Loader::loadClass("Bral_Voir_Communaute");
 		return new Bral_Voir_Communaute($request, $view);
@@ -38,7 +38,7 @@ class Bral_Voir_Factory {
 		Zend_Loader::loadClass("Bral_Voir_Materiel");
 		return new Bral_Voir_Materiel($request, $view);
 	}
-	
+
 	static function getEquipement($request, $view) {
 		Zend_Loader::loadClass("Bral_Voir_Equipement");
 		return new Bral_Voir_Equipement($request, $view);
@@ -48,22 +48,22 @@ class Bral_Voir_Factory {
 		Zend_Loader::loadClass("Bral_Voir_Potion");
 		return new Bral_Voir_Potion($request, $view);
 	}
-	
+
 	static function getRune($request, $view) {
 		Zend_Loader::loadClass("Bral_Voir_Rune");
 		return new Bral_Voir_Rune($request, $view);
 	}
-	
+
 	static function getBraldun($request, $view) {
 		Zend_Loader::loadClass("Bral_Voir_Braldun");
 		return new Bral_Voir_Braldun($request, $view);
 	}
-	
+
 	static function getBralduns($request, $view) {
 		Zend_Loader::loadClass("Bral_Voir_Bralduns");
 		return new Bral_Voir_Bralduns($request, $view);
 	}
-	
+
 	static function getCommunautes($request, $view) {
 		Zend_Loader::loadClass("Bral_Voir_Communautes");
 		return new Bral_Voir_Communautes($request, $view);
@@ -73,7 +73,7 @@ class Bral_Voir_Factory {
 		Zend_Loader::loadClass("Bral_Voir_Monstre");
 		return new Bral_Voir_Monstre($request, $view);
 	}
-	
+
 	static function getVue($request, $view) {
 		Zend_Loader::loadClass("Bral_Voir_Vue");
 		return new Bral_Voir_Vue($request, $view);
